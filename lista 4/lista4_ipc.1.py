19cad2efc856a09cb0b5aa3ec1d567349948e849
Mateus Souza <mota.mateus@live.com>
2016-04-01 07:19:17 -0400

    ipc_lista4.1.py
    Adicionei comentários para o entendimento dos próximos que venham ver o código

--- lista 4/lista4_ipc.1.py
+++ lista 4/lista4_ipc.1.py
@@ -1,19 +1,19 @@
 
 #Beatriz Pessoa Longato 1615310001
 
-numeros= []
+numeros= [] # Vetor que irá receber os números digitados
 
-cont=1
+cont=1 # Contador para ser incrementado durante o looping
 
-while cont <=5:
+while cont <=5: # Enquanto esta condição for verdadeira...
     
-    n=int(raw_input("Digite um número: "))
+    n=int(raw_input("Digite um número: ")) " ... Irá solicitar ao usuário um número
 
-    numeros.append(n)
+    numeros.append(n) # Função que adiciona os termos digitados pelo usuário no vetor 'numeros'
     
-    cont +=1
+    cont +=1 # Incrementando contador para não ficar num looping
     
-print (numeros)
+print (numeros) # Imprimirá ao usuário o vetor 'numeros' com os caracteres informados anteriormente pelo usuário
 
 
 
