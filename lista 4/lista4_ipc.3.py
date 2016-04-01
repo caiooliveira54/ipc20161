7ed50492eb299ceb57bca78d46032df8e0b456cb
Mateus Souza <mota.mateus@live.com>
2016-04-01 07:30:24 -0400

    ipc_lista4.3.py
    Comentários adicionados para o entendimento dos próximos que vierem a ler

--- lista 4/lista4_ipc.3.py
+++ lista 4/lista4_ipc.3.py
@@ -1,19 +1,19 @@
 #Beatriz Pessoa Longato 1615310001
 
-notas = []
+notas = [] # Vetor onde serão armazenada as notas informadas
 
-quantidade_notas= 4
+quantidade_notas= 4 # Limitador para condição
 
-media = 0
+media = 0 # Média funcionará como acumulador, somará todos os valores informados abaixo do "For"
 
-for i in range (quantidade_notas):
+for i in range (quantidade_notas): # Aqui será realizada uma verificação com 'i' sendo o índice até ele ter 4 índices. (0 - 3)
     
-    notas.append(float(raw_input("Digite a nota: ")))
+    notas.append(float(raw_input("Digite a nota: "))) # Função que solicitará as notas do usuário enquanto o 'For' for estiver no alcance
     
-    media = media + notas[i]
-    
-media = media / quantidade_notas
+    media = media + notas[i] # Acumulador das notas informadas acima, ex.: Primeiramente digitel nota 10, ficará no indice i = 0
+                             # assim sucessivamente, acumulará as notas que forem digitadas a cada looping
+media = media / quantidade_notas # Obviamente pegará o acumulador com todas as 4 notas e dividirá pela quantidade de notas
 
-print (notas)
+print (notas) # Irá imprimir a lista 'notas'
 
-print "A média é" , (media)
+print "A média é" , (media) # Irá imprimir a média das notas
