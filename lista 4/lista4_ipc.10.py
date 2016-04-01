935f6921dee04376efca8057f1d9eba0eb30f79c
Mateus Souza <mota.mateus@live.com>
2016-04-01 09:45:44 -0400

    ipc_lista4.10.py

--- lista 4/lista4_ipc.10.py
+++ lista 4/lista4_ipc.10.py
@@ -1,36 +1,36 @@
 #Beatriz Pessoa Longato 1615310001
 
-a=[]
+a=[] # Vetor que receberá inicialmente os valores
 
-b=[]
+b=[] # Vetor que receberá posterior ao 'a' os valores
 
-c=[]
+c=[] # Vetor que receberá o vetor 'a' + 'b' intercalando valores de cada um
 
-quantidade_elementos=10
+quantidade_elementos=10 # Delimita quantos números deverão ter cada vetor
 
 
-for i in range(quantidade_elementos):
+for i in range(quantidade_elementos): # Enquanto o índice 'i' pertencer ao alcance de 0 até 9, ele irá...
     
-    n=float(raw_input("Digite um número para a lista A: "))
+    n=float(raw_input("Digite um número para a lista A: ")) # Solicitará valores para n
     
-    a.append(n)
+    a.append(n) # Armazenará os valores de 'n' ao vetor 'a'
     
-for i in range (quantidade_elementos):
+for i in range (quantidade_elementos): # Enquanto o índice 'i' pertencer ao alcance de 0 até 9, ele irá...
     
-    n2=float(raw_input("Digite um número para a lista B: "))
+    n2=float(raw_input("Digite um número para a lista B: ")) # Solicitará valores para n2
     
-    b.append(n2)
+    b.append(n2) # Armazenará os valores de 'n' ao vetor 'b'
     
-for i,y in zip (a,b):
+for i,y in zip (a,b): # Nesta função aqui (zip), ele intercalará valores de 'a' e 'b' no vetor final 'c'
     
-    c.append(i)
-    c.append(y)
+    c.append(i) # Acrescentará os valores respectivos aos índices do vetor 'a' no 'c'
+    c.append(y) # Logo após o acima, acrescentará os valores respectivos aos índices do 'b' no 'c'
+                # Ou seja, intercalando...
     
-    
-print (a) 
-print (b)
+print (a) # Imprimirá ao usuário o vetor 'a'
+print (b) # Imprimirá ao usuário o vetor 'b'
 print "Lista resultante intercalada:"
-print (c) 
+print (c) # Imprimirá ao usuário o vetor 'c'
 
     
     
