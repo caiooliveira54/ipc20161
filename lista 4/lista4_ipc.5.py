888d98828763e682008a9d53fde7233fea3ce947
Mateus Souza <mota.mateus@live.com>
2016-04-01 07:59:28 -0400

    ipc_lista4.5.py

--- lista 4/lista4_ipc.5.py
+++ lista 4/lista4_ipc.5.py
@@ -1,35 +1,35 @@
 #Beatriz Pessoa Longato 1615310001
 
-numeros= []
+numeros= [] # Lista que armazenará todos os números informados pelo usuário 
 
-par= []
+par= [] # Lista que armazenará todos os números pares informados pelo usuário 
 
-impar=[]
+impar=[] # Lista que armazenará todos os números impares informados pelo usuário 
 
-cont= 1
+cont= 1 # Contador para ser utilizado na condição até desobedecer a condição
 
-while cont <= 20:
+while cont <= 20: # Enquanto o contador obedecer a condição...
     
-    n=int(raw_input("Digite um número: "))
+    n=int(raw_input("Digite um número: ")) # Solicitará um número ao usuário, e...
     
-    numeros.append(n)
+    numeros.append(n) # ... por consequência armazenado na lista 'numeros'
     
-    cont +=1
+    cont +=1 # Contador sendo incrementado a cada número informado e armazenado
     
-    if n % 2 == 0:
+    if n % 2 == 0: # Se o resto da divisão do número informado por 2 for zero, será considerado como par...
         
-        par.append(n)
+        par.append(n) #... e armazenado na lista 'par'
         
-    else:
+    else: # Caso não seja verdade a condição acima, cairá nesta daqui e será ímpar...
         
-        impar.append(n)
+        impar.append(n) # ... e armazenado aqui
         
         
-print (numeros)
+print (numeros) # Imprimirá ao usuário o vetor 'numeros'
 
-print(par)
+print(par) # Imprimirá ao usuário o vetor 'par'
 
-print(impar)
+print(impar) # Imprimirá ao usuário o vetor 'impar'
     
     
     
