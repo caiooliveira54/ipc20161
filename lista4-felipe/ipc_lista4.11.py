9e3e271b5dbc15a0f694454349d306e72002f999
Mateus Souza <mota.mateus@live.com>
2016-04-04 10:46:28 -0400

    Update ipc_lista4.11.py

--- lista4-felipe/ipc_lista4.11.py
+++ lista4-felipe/ipc_lista4.11.py
@@ -2,15 +2,15 @@
 #Programa Lista 4, questão 11;
 #Felipe Henrique Bastos Costa - 1615310032;
 #
-lista1 = []
-lista2 = []
-lista3 = []
-lista4 = []
-v1 = 10
-cont1 = 0
+lista1 = [] # Primeiro vetor
+lista2 = [] # Segundo vetor
+lista3 = [] # Terceiro vetor
+lista4 = [] # Quarto vetor que receberá os valores dos vetores acima de forma alternada
+v1 = 10 # Limite para cada vetor
+cont1 = 0 
 cont2 = 1
 
-while(cont1 < v1):
+while(cont1 < v1): # Enquanto verdade solicitará números para serem adicionados ao vetor 1, 2 e 3 e serão acrescidos de forma alternada no vetor 4
     x1 = raw_input("Informe o %dº elemento a ser colocado na 1º lista:\n"%cont2)
     x2 = raw_input("Informe o %dº elemento a ser colocado na 2º lista:\n"%cont2)
     x3 = raw_input("Informe o %dº elemento a ser colocado na 3º lista:\n"%cont2)
@@ -23,4 +23,4 @@ while(cont1 < v1):
     cont1+=1
     cont2+=1
 
-print("A uniao das listas de forma alternada e:\n%s"%lista4)
+print("A uniao das listas de forma alternada e:\n%s"%lista4) # Resultado do vetor 4
