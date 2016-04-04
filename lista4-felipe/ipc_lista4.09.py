267e65170dc0184e599b470b8f313c6acbf24bd0
Mateus Souza <mota.mateus@live.com>
2016-04-04 11:12:25 -0400

    Update ipc_lista4.09.py

--- lista4-felipe/ipc_lista4.09.py
+++ lista4-felipe/ipc_lista4.09.py
@@ -2,15 +2,15 @@
 #Programa Lista 4, questão 9;
 #Felipe Henrique Bastos Costa - 1615310032;
 #
-lista = []
-v = 10
-cont1 = 0
+lista = [] # Vetor que receberá os 10 números
+v = 10 # Limite para o contador
+cont1 = 0 
 cont2 = 1
-soma = 0
+soma = 0 # Acumulador para somar os números
 while(cont1 < v):
-    x = int(input("Informe o %dº numero inteiro da sua lista:\n"%cont2))
-    lista.append(x)
-    soma = soma + (x**2)
+    x = int(input("Informe o %dº numero inteiro da sua lista:\n"%cont2)) # Solicitará os valores enquanto for 'True'
+    lista.append(x) # Adicionará ao vetor os numeros
+    soma = soma + (x**2) # Elevará os números ao quadrado
     cont1+=1
     cont2+=1
-print(soma)
+print(soma) # Imprimirá a soma dos quadrados
