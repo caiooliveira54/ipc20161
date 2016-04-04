a41d666b864c199e6183f562214f34a2741382da
Mateus Souza <mota.mateus@live.com>
2016-04-04 10:43:38 -0400

    Update ipc_lista4.08.py

--- lista4-felipe/ipc_lista4.08.py
+++ lista4-felipe/ipc_lista4.08.py
@@ -2,23 +2,23 @@
 #Programa Lista 4, questão 8;
 #Felipe Henrique Bastos Costa - 1615310032;
 #
-idade = []
-inverso_i = []
-altura = []
-inverso_a = []
-v = 5
+idade = [] # Vetor que armazenará todas as idades inseridas pelo usuário
+inverso_i = [] # Vetor que armazenará todas as idades invertidas
+altura = [] # Vetor que receberá todas as alturas inseridas pelo usuário
+inverso_a = [] # Vetor que receberá todas as alturas invertidas
+v = 5 # Limite de pessoas
 cont1 = 0
 cont2 = 1
 
-while(cont1 < v):
+while(cont1 < v): # Equanto for menor que 5 solicitará os valores x e y
     x = int(input("Informe a idade da %dº pessoa:\n"%cont2))
     y = float(input("Informe a altura da %dº pessoa:\n"%cont2))
-    idade.append(x)
-    altura.append(y)
+    idade.append(x) # Adicionará x ao vetor idade
+    altura.append(y) # Adicionará y ao vetor altura
     cont1 += 1
     cont2 += 1
 
-while(v > 0):
+while(v > 0): # Aqui será invertida as listas idade e altura
     v-=1
     w = idade[v]
     z = altura[v]
