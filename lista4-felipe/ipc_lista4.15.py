f3c51b0889bf7182d839d05551c1a29bcb9436ac
Mateus Souza <mota.mateus@live.com>
2016-04-04 10:29:11 -0400

    Update ipc_lista4.15.py

--- lista4-felipe/ipc_lista4.15.py
+++ lista4-felipe/ipc_lista4.15.py
@@ -2,37 +2,37 @@
 #Programa Lista 4, questão 15;
 #Felipe Henrique Bastos Costa - 1615310032;
 #
-lista = []
-listainv = []
-cond = True
-cont1 = 0
+lista = [] # Vetor que vai receber as notas inseridas pelo usuário
+listainv = [] # Vetor que vai receber as notas inseridas da forma inversa
+cond = True # Condição para tornar o 'while' um loop até ser quebrado com o -1
+cont1 = 0 
 cont2 = 0
 cont3 = 0
 cont4 = 0
-soma = 0
+soma = 0 # Receberá todos os valores e somará-los
 
-while(cond):
-    nota = float(input("Informe uma nota(para encerrar o programa, informe -1):\n"))
-    if(nota == -1):
+while(cond): # Enquanto for verdade 
+    nota = float(input("Informe uma nota(para encerrar o programa, informe -1):\n")) # Solicitará notas
+    if(nota == -1): # Se for -1, encerrará o programa
         soma = soma
         cont1 = cont1
         cond = False
-    else:
+    else: # Caso não, adicionará nota ao vetor lista e somará os valores e o contador será incrementado 
         lista.append(nota)
         soma += nota
         cont1 += 1
-media_notas = (soma)/cont1
-v = cont1 - 1
+media_notas = (soma)/cont1 # 
+v = cont1 - 1 # Último elemento do vetor lista
 
-while(cont2 <= v):
-    x = lista[v]
+while(cont2 <= v): # Enquanto for verdadeira
+    x = lista[v] # Adicionará os últimos números do vetor lista ao vetor listainv
     listainv.append(x)
-    v -= 1
+    v -= 1 # Decrementando o contador
     
-for i in range(cont1):
-    if(lista[i] > media_notas):
+for i in range(cont1): # Verificará no vetor...
+    if(lista[i] > media_notas):  # Quantos números estão acima da média
         cont3 += 1
-    if(lista[i] < 7):
+    if(lista[i] < 7): # Quantos números são menores que sete
         cont4 += 1
 print("A quantidade de valores lidos foi de %d"%cont1)
 print("Os valores informados foram:\n%s"%lista)
