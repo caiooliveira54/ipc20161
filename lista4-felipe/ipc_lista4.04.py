10430e688373e90da0abb5ab05337fa31bb6c2cf
Mateus Souza <mota.mateus@live.com>
2016-04-04 10:12:09 -0400

    Update ipc_lista4.04.py

--- lista4-felipe/ipc_lista4.04.py
+++ lista4-felipe/ipc_lista4.04.py
@@ -2,20 +2,18 @@
 #Programa Lista 4, questão 4;
 #Felipe Henrique Bastos Costa - 1615310032;
 #
-texto = raw_input("Informe uma palavra ou frase de 10 caracteres, espaços incluidos:\n")
-cont = 0
-v = 10
-j = v
-soma = 0
-lista = []
+texto = raw_input("Informe uma palavra ou frase de 10 caracteres, espaços incluidos:\n") # Solicita ao usuário o texto de 10 caracteres
+cont = 0 # Contador para ser incrementado e funcionará como índice da string
+v = 10 # Limitador do contador
+soma = 0 # Acumulador para quantidade de consoantes
+lista = [] # Vetor que receberá todas as consoantes
 
 while(cont < v):
-    j-=1
     if(texto[cont] == "a" or texto[cont] == "e" or texto[cont] == "i" or texto[cont] == "o" or texto[cont] == "u" or texto[cont] == " "):
-        soma = soma
-    else:
+        soma = soma # Acima temos uma condição para ser vogal e ao lado temos a soma sendo ela mesma, pois não haverá consoante nessa condição
+    else: # Caso não caia na condição acima, será acrescido a soma mais um e a consoante será adicionada à lista
         soma+=1
         x = texto[cont]
         lista.append(x)
-    cont+=1
-print("exitem %d consoantes, sendo elas as seguintes:\n%s"%(soma,lista))
+    cont+=1 # Incrementando o contador para evitar loop
+print("exitem %d consoantes, sendo elas as seguintes:\n%s"%(soma,lista)) # Impressão da lista com consoantes e a quantidade delas
