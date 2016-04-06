b30187e07530ec27823ddf644e8074ba0d12d3db
caniiggia <caniiggia2010@hotmail.com>
2016-04-06 15:29:32 -0400

    Update pergunta10.py

--- Bruno/questoes lista 1,2,3/list 1/pergunta10.py
+++ Bruno/questoes lista 1,2,3/list 1/pergunta10.py
@@ -1,3 +1,4 @@
+#Bruno de Olliveira Freire - 1615310030
 c=float(input("qual a temperatura em celsius?:\n"))
 f=(9*c)/5 + 32
-print("em farenheit essa temperatura é equivalente a:\n%.2f"%f)
+print("em farenheit essa temperatura Ã© equivalente a:\n%.2f"%f)
