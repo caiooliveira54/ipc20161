d35db8782aa59f8ff3d3035ea5cd24f6dbcce46a
Felipe Henrique <solelipecosta1@gmail.com>
2016-04-06 17:04:10 -0400

    Added files via upload

---
+++ lista4-oficial/ipc.lista4.8.py
@@ -0,0 +1,33 @@
+"""
+lista 4 questao 8:
+Faça um Programa que peça a idade e a altura de 5 pessoas, 
+armazene cada informação no seu respectivo vetor. 
+Imprima a idade e a altura na ordem inversa a ordem lida.
+"""
+#Luiz Gustavo Rocha Melo - 1615310015
+altura = []
+alturainv = []
+idade = []
+idadeinv = []
+v = 5
+c1 = 1
+c2 = 0
+
+while (c2 < v):
+    x = int(input("A idade da pessoa: "))
+    idade.append(x)
+    y = float(input("O tamnaho da pessoa: "))
+    altura.append(y)
+    c1 += 1
+    c2 += 1
+    
+while (v > 0):
+    v -= 1
+    w = idade[v]
+    z = altura [v]
+    
+    idadeinv.append(w)
+    alturainv.append(z)
+
+print("A ordem inversa da idade",idadeinv)
+print("A ordem inversa da altura",alturainv)
---
+++ lista4-oficial/ipc.lista4.9.py
@@ -0,0 +1,20 @@
+"""
+lista 4 questao 9
+Faça um Programa que leia um vetor A com 10 números inteiros, 
+calcule e mostre a soma dos quadrados dos elementos do vetor.
+"""
+#Luiz Gustavo Rocha Melo - 1615310015
+numeros = []
+v = 10
+c1 = 0
+c2 = 1
+soma = 0
+
+while(c1 < v):
+    x = int(input("informe um numero inteiro: "))
+    numeros.append
+    soma = soma + (x**2)
+    c1 += 1
+    c2 += 1
+
+print("A soma dos quadrados dos elementos e", soma)
