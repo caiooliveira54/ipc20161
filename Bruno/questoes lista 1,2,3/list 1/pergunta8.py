9b8106e13a88d703d8d4c045d8e8616c085797db
caniiggia <caniiggia2010@hotmail.com>
2016-04-06 15:34:19 -0400

    Update pergunta8.py

--- Bruno/questoes lista 1,2,3/list 1/pergunta8.py
+++ Bruno/questoes lista 1,2,3/list 1/pergunta8.py
@@ -1,3 +1,4 @@
+#Bruno de Olliveira Freire - 1615310030
 ganho_h=float(input("quanto voce ganha por hora de trabalho?:\n"))
 hora_mes=float(input("quantas horas voce trabalha por mes?:\n"))
 ganho=ganho_h*hora_mes
