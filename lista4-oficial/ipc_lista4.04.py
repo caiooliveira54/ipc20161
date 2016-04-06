d499094ba1e27e44b9c751ec6d4a10499f54f9d6
vurmund <zenkichialex@outlook.com>
2016-04-06 13:09:28 -0400

    Questao 4 oficial

---
+++ lista4-oficial/ipc_lista4.04.py
@@ -0,0 +1,13 @@
+A = ["a","b","c","d","e","f","g","h","i","j"]
+C = 0
+v = len(A)
+ac = 0
+while C < len(A):
+    if A[C] != "a" and A[C] != "e" and A[C] != "i" and A[C] != "o" and A[C] != "u":
+        ac = ac + 1
+        print(A[C])
+    C = C + 1
+print("O numero de consoantes são: ",ac)
+     
+    
+     
