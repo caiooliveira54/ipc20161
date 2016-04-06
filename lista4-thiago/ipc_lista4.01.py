663db8ff80da0c72a0ba9b16f2880765e756b12e
tsbecn16 <tsb.ecn16@uea.edu.br>
2016-04-06 00:42:32 -0400

    Atualizando

--- lista4-thiago/ipc_lista4.01.py
+++ lista4-thiago/ipc_lista4.01.py
@@ -1,3 +1,6 @@
+#ipc_lista4.01
+#Thiago Santos Borges - Matrícula - 1615310023
+#
 vetor = []
 for i in range(1,6):
     numero = int(input("Digite numero:"))
--- lista4-thiago/ipc_lista4.04.py
+++ lista4-thiago/ipc_lista4.04.py
@@ -1,3 +1,6 @@
+#ipc_lista4.01
+#Thiago Santos Borges - Matrícula - 1615310023
+#
 consoantes = []
 letras = []
 acm = 0
