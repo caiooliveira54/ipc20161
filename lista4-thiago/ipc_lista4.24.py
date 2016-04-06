8d74a257e9d665c5d66b299f10e938607ed4b7fd
tsbecn16 <tsb.ecn16@uea.edu.br>
2016-04-06 13:51:18 -0400

    Update ipc_lista4.24.py

--- lista4-thiago/ipc_lista4.24.py
+++ lista4-thiago/ipc_lista4.24.py
@@ -1,7 +1,7 @@
 """Faça um programa que simule um lançamento de dados. Lance o dado 100 vezes e armazene os resultados em um vetor .
 Depois, mostre quantas vezes cada valor foi conseguido.
 Dica: use um vetor de contadores(1-6) e uma função para gerar numeros aleatórios, simulando os lançamentos dos dados."""
-#ipc_lista4.01
+#ipc_lista4.24
 #Thiago Santos Borges - Matrícula - 1615310023
 #
 import random
