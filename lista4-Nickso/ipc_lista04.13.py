7238ecf92410672050083c2b1fcfd0b271e98db5
nicksocalheiros <nickso_facanha@hotmail.com>
2016-04-06 20:25:47 -0400

    Lista de Exercicios4

---
+++ lista4-Nickso/ipc_lista04.13.py
@@ -0,0 +1,20 @@
+# Nickso Parick Façanha Calheiros
+temp = []
+mes = ["Janeiros", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outeubro", "Novembro", "Dezembro"]
+armaz = []
+armazmes = []
+
+cont = 0
+acum = 0
+ind = len(mes)
+
+while cont < ind:
+    temperatura = float(input("Digite a temperatura de %s" %mes[cont]))
+    temp.append(temperatura)
+    acum += temperatura
+    cont += 1
+    media = acum / len(temp)
+
+for i in range(12):
+    if temp[i]>media:
+        print("Temperatura acima da media %5.2f-%s" %(temp[i],mes[i]))
