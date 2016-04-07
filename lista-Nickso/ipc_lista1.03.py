2e7117e8d3b3b62e3577976dc71ae7bc52b9400c
nicksocalheiros <nickso_facanha@hotmail.com>
2016-04-06 21:54:53 -0400

    exercicio lista 1

---
+++ lista-Nickso/ipc_lista1.03.py
@@ -0,0 +1,10 @@
+#Nickso Patrick Façanha Calheiros - 1615310059
+#
+#
+#
+numero1 = float(input("Digite um numero: "))
+numero2 = float(input("Digite outro numero "))
+
+soma = numero1 + numero2
+
+print ("Resposta %d" %soma)
