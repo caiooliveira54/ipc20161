89b7d7821656b44de3e4ffb8022696bcf574dc52
nicksocalheiros <nickso_facanha@hotmail.com>
2016-04-08 03:20:19 -0400

    exercicio lista 1

---
+++ lista-Nickso/ipc_lista1.04.py
@@ -0,0 +1,12 @@
+#Nickso Patrick Façanha Calheiros - 1615310059
+#
+#
+#
+bimestre1 = float(input("Nota do Primeiro Bimestre: "))
+bimestre2 = float(input("Nota do Segundo Bimestre: "))
+bimestre3 = float(input("Nota do Terceiro Bimestre: "))
+bimestre4 = float(input("nota do Quarto Bimestre: "))
+
+media = (bimestre1 + bimestre2 + bimestre3 + bimestre4)/4
+
+print ("Sua Media e %2.2f" %media) 
