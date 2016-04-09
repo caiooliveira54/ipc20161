d135e9ce436cb04cc18fc816de5b46f40ec01dbe
LuixGustavo <lgrm.ecn16@uea.edu.br>
2016-04-09 13:42:28 -0400

    Update ipc_lista4.09.py

--- lista4-oficial/ipc_lista4.09.py
+++ lista4-oficial/ipc_lista4.09.py
@@ -4,7 +4,7 @@ Faça um Programa que leia um vetor A com 10 números inteiros,
 calcule e mostre a soma dos quadrados dos elementos do vetor.
 """
 #Luiz Gustavo Rocha Melo - 1615310015
-numeros = []
+vetor_A = []
 v = 10
 c1 = 0
 c2 = 1
@@ -12,9 +12,9 @@ soma = 0
 
 while(c1 < v):
     x = int(input("informe um numero inteiro: "))
-    numeros.append
+    vetor_A.append(x)
     soma = soma + (x**2)
     c1 += 1
     c2 += 1
 
-print("A soma dos quadrados dos elementos e", soma)
+print("A soma dos quadrados dos elementos e igual a:", soma)
