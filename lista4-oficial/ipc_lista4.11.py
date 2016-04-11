afcaf869a1db133a05c1c35a5ad59d9fba2b3706
LoreneMarques <ldsm.ecn16@uea.edu.br>
2016-04-11 15:36:36 -0400

    Added files via upload

--- lista4-oficial/ipc_lista4.11.py
+++ lista4-oficial/ipc_lista4.11.py
@@ -1,12 +1,25 @@
 #Lorene Marques - 1615310013
-v1 = [0,3,6,9,12,15,18,21,24,27]
-v2 = [1,4,7,10,13,16,19,22,25,28]
-v3 = [2,5,8,11,14,17,20,23,26,29]
+v1 = []
+v2 = []
+v3 = []
 v4 = []
 x = 0
+print("Primeiro vetor")
+for i in range(0,10):
+    num1 = input("Informe um elemento: ")
+    v1.append(num1)
+print("Segundo vetor")
+for i in range(0,10):
+    num2 = input("Informe um elemento: ")
+    v2.append(num2)
+print("Terceiro vetor")
+for i in range(0,10):
+    num3 = input("Informe um elemento: ")
+    v3.append(num3)
 while x < 10:
     v4.append(v1[x])
     v4.append(v2[x])
     v4.append(v3[x])
     x += 1
+print("O vetor intercalado: ")
 print(v4)
