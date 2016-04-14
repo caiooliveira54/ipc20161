cc849378eedd7d3c0baf69a4e5748b04dea2d52f
arielrocha1 <ariel_rocha@live.com>
2016-04-14 18:00:04 -0400

    Update ipc_lista1.04.py

--- lista1/ipc_lista1.04.py
+++ lista1/ipc_lista1.04.py
@@ -6,10 +6,10 @@
 
 #Faça um Programa que peça as 4 notas bimestrais e mostre a media
 
-nota1 = int(input("Digite a primeira nota do bimestre: "))
-nota2 = int(input("Digite a segunda nota do bimestre: "))
-nota3 = int(input("Digite a terceira nota do bimestre: "))
-nota4 = int(input("Digite a quarta nota do bimestre: "))
+nota1 = float(input("Digite a primeira nota do bimestre: "))
+nota2 = float(input("Digite a segunda nota do bimestre: "))
+nota3 = float(input("Digite a terceira nota do bimestre: "))
+nota4 = float(input("Digite a quarta nota do bimestre: "))
 
 media = (nota1 + nota2 + nota3 + nota4)/ 4.0
 
