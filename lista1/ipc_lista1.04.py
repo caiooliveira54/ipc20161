68deb02eb99651f1ea86c42554ab8d6bd877cb61
arielrocha1 <ariel_rocha@live.com>
2016-04-14 17:53:51 -0400

    Update ipc_lista1.04.py

--- lista1/ipc_lista1.04.py
+++ lista1/ipc_lista1.04.py
@@ -2,20 +2,15 @@
 #Any Mendes Carvalho - 1615310044
 #Ariel Guilherme Rocha Capistrano - 1615310029
 #Beatriz Pessoa Longato - 1615310001
-#Nickso Patrick Façanha Calheiros - 1615310059
+#Nickso Patrick FaÃ§anha Calheiros - 1615310059
 
-#Faça um Programa que peça as 4 notas bimestrais e mostre a media
+#FaÃ§a um Programa que peÃ§a as 4 notas bimestrais e mostre a media
 
 nota1 = int(input("Digite a primeira nota do bimestre: "))
 nota2 = int(input("Digite a segunda nota do bimestre: "))
 nota3 = int(input("Digite a terceira nota do bimestre: "))
 nota4 = int(input("Digite a quarta nota do bimestre: "))
 
-print
-media = (nota1+nota2+nota3+nota4)/ 4.0
+media = (nota1 + nota2 + nota3 + nota4)/ 4.0
 
-
-print "A sua media e: %s" %media
-
-
-
+print("A sua media e: %.2f" %media)
