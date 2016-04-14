4aaedc2343929670d9245c373df989cd6e2dac74
arielrocha1 <ariel_rocha@live.com>
2016-04-14 17:51:00 -0400

    Update ipc_lista1.03.py

--- lista1/ipc_lista1.03.py
+++ lista1/ipc_lista1.03.py
@@ -2,10 +2,11 @@
 #Any Mendes Carvalho - 1615310044
 #Ariel Guilherme Rocha Capistrano - 1615310029
 #Beatriz Pessoa Longato - 1615310001
-#Nickso Patrick Façanha Calheiros - 1615310059
+#Nickso Patrick FaÃ§anha Calheiros - 1615310059
 
-#Faça um Programa que peça dois números e imprima a soma
+#FaÃ§a um Programa que peÃ§a dois nÃºmeros e imprima a soma
 
-number1 =input("Primeiro numero: ")
-number2 =input("Segundo numero: ")
+number1 = input("Primeiro numero: ")
+number2 = input("Segundo numero: ")
+
 print(number1+number2)
