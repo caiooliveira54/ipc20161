f55a67bef05a1b0862a8bd8fcca5d0394d8977fe
AbFrota <beatrizffrotaa@gmail.com>
2016-04-14 00:01:59 -0300

    Update ipc_lista4.09.py

--- lista4/ipc_lista4.09.py
+++ lista4/ipc_lista4.09.py
@@ -3,7 +3,15 @@ lista 4 questao 9
 Faça um Programa que leia um vetor A com 10 números inteiros, 
 calcule e mostre a soma dos quadrados dos elementos do vetor.
 """
+#EQUIPE 2
+#ANA BEATRIZ FROTA - 1615310027
+#
+#
+#
+#
 #Luiz Gustavo Rocha Melo - 1615310015
+
+
 vetor_A = []
 v = 10
 c1 = 0
