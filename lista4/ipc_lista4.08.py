d7091b43fd483de9f6993a070698f24067bebbba
AbFrota <beatrizffrotaa@gmail.com>
2016-04-15 13:30:50 -0300

    Update ipc_lista4.08.py

--- lista4/ipc_lista4.08.py
+++ lista4/ipc_lista4.08.py
@@ -8,7 +8,12 @@ Imprima a idade e a altura na ordem inversa a ordem lida.
 #ANA BEATRIZ FROTA - 1615310027
 #
 #
+#
+#
+#
 #Luiz Gustavo Rocha Melo - 1615310015
+
+
 altura = [] #vetor para altura
 alturainv = [] #vetor para a altura na ordem inversa
 idade = [] #vetor para idade
@@ -17,11 +22,11 @@ v = 5 #variável para o indice
 c1 = #contador
 
 while (c1 < v): 
-    x = int(input("A idade da pessoa: "))
-    idade.append(x) 
-    y = float(input("A altura da pessoa: "))
-    altura.append(y)
-    c1 += 1
+    x = int(input("A idade da pessoa: ")) # X RECEBE O VALOR DA IDADE
+    idade.append(x) #VETOR RECEBE DO VALOR DE X
+    y = float(input("A altura da pessoa: ")) # Y RECEBE O VALOR DA ALTURA/
+    altura.append(y) # VETOR ALTURA RECEBE O VALOR DE Y
+    c1 += 1 # CONTADOR MAIS 1
     
 while (v > 0):
     v -= 1
