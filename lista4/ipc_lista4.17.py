a775c9c91b9e4f76128c21c0eae8f6819fbd5078
AbFrota <beatrizffrotaa@gmail.com>
2016-04-15 15:30:16 -0300

    Update ipc_lista4.17.py

--- lista4/ipc_lista4.17.py
+++ lista4/ipc_lista4.17.py
@@ -1,9 +1,16 @@
 
-#Grupo: 2
-# Lucas Ferreira Soares - 1615310014
-# Ana Beatriz Frota - 1615310027
-# 
-#Lista: 4
+#
+#introdução a programação de computadores
+#Professor: Jucimar JR
+#EQUIPE 2
+#
+#Ana Beatriz Frota  - 1615310027 
+#Kylciane Cristiny Lopes Freitas - 1615310052
+#Frankilin Yuri Gonçaves dos santos - 1615310033
+#Lucas Ferreira Soares - 1615310014
+#Luiz Gustavo Rocha Melo - 1615310015
+#
+#
 #Questão: 17
 
 vet = []
