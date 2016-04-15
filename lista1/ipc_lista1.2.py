8f0cec27453ac927748b5f7cee56285ccb41e35c
Kid Mendes <kid.mon@hotmail.com>
2016-04-15 19:45:15 -0400

    Update ipc_lista1.2.py

--- lista1/ipc_lista1.2.py
+++ lista1/ipc_lista1.2.py
@@ -1,9 +1,11 @@
-#ipc_lista1.2
+#Introdução a Programação de Computadores
 #Professor: Jucimar Junior
 #Any Mendes Carvalho - 1615310044
-#
-#
-#
+#Kid Mendes de Oliveira Neto - 1615310011
+#Victor Rafael da Silva e Silva - 1615310025
+#Eduardo Maia Freire - 1615310003
+#Luiz Alexandre Olivera de Souza-1615310057
+#Matheus Palheta Barbosa -1615310019
 #
 
 
