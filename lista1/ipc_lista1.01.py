bb693c539d062e11cb2fa16dbd77d60cba7a0af6
any1m1c <anymendescarvalho@gmail.com>
2016-04-15 14:05:15 -0300

    Colocar nome de cada membro da equipe

--- lista1/ipc_lista1.01.py
+++ lista1/ipc_lista1.01.py
@@ -1,10 +1,11 @@
 
-#Lista de Estrutura Sequencial
+#ipc_lista1.1
 #Professor: Jucimar Junior
-#Alexander Emerson Batalha Carlos - 1615310051
 #Any Mendes Carvalho - 1615310044
-#Ariel Guilherme Rocha Capistrano - 1615310029
-#Beatriz Pessoa Longato - 1615310001
-#Nickso Patrick Façanha Calheiros - 1615310059
+#
+#
+#
+#
+#Faça um Programa que mostre a mensagem "Alo mundo" na tela. 
 
 print("Hello World")
