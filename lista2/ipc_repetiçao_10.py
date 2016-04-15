6c859c3f2302c848edd8fe25b95f575e6d8030d1
AbFrota <beatrizffrotaa@gmail.com>
2016-04-15 11:20:15 -0400

    Added files via upload

---
+++ lista2/ipc_repetiçao_10.py
@@ -0,0 +1,10 @@
+numero1 = int(input("Insira um numero: "))
+numero2 = int(input("Insira outro numero: "))
+
+while (numero1<(numero2 - 1)):
+    numero1 += 1
+    print (numero1)
+
+while (numero2 <(numero1 - 1)):
+    numero2 += 1
+    print (numero2)
---
+++ lista2/ipc_repetiçao_9.py
@@ -0,0 +1,8 @@
+cont = 1
+numero = 1
+
+while (cont <= 49):
+    numero= numero + 1
+    cont = cont + 1
+    if (numero%2 !=0):
+        print(numero)
