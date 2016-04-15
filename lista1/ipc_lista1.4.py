e07e3488e01a0b7b941eeb86ce69f21e2058d815
Kid Mendes <kid.mon@hotmail.com>
2016-04-15 19:49:23 -0400

    Update ipc_lista1.4.py

--- lista1/ipc_lista1.4.py
+++ lista1/ipc_lista1.4.py
@@ -1,5 +1,22 @@
-#ipc_lista1.4
+#Introdução a Programação de Computadores
 #Professor: Jucimar Junior
-#Any Mendes Carvalho 
+#Any Mendes Carvalho - 1615310044
+#Kid Mendes de Oliveira Neto - 1615310011
+#Victor Rafael da Silva e Silva - 1615310025
+#Eduardo Maia Freire - 1615310003
+#Luiz Alexandre Olivera de Souza-1615310057
+#Matheus Palheta Barbosa -1615310019
+#
 #Faça um Programa que peça as 4 notas bimestrais e mostre a media
+#
 
+nota1 = float(input("Digite a primeira nota do bimestre: "))
+nota2 = float(input("Digite a segunda nota do bimestre: "))
+nota3 = float(input("Digite a terceira nota do bimestre: "))
+nota4 = float(input("Digite a quarta nota do bimestre: "))
+
+print
+media = (nota1+nota2+nota3+nota4)/ 4.0
+
+
+print "A sua media e: %s" %media
