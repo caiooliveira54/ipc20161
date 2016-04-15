ee18647cbaa97302aae833eb64224db7992522cd
AbFrota <beatrizffrotaa@gmail.com>
2016-04-15 13:18:59 -0300

    Update ipc_lista2.10.py

--- lista2/ipc_lista2.10.py
+++ lista2/ipc_lista2.10.py
@@ -8,11 +8,11 @@
 #
 #
 
-numero1 = int(input("Insira um numero: "))
-numero2 = int(input("Insira outro numero: "))
+numero1 = int(input("Insira um numero: ")) #PEDE UM NUMERO PARA INICIAR A CONTAGEM
+numero2 = int(input("Insira outro numero: ")) #PEDE UM NUMERO PARA TERMINAR A CONTAGEM
 
-while (numero1<(numero2 - 1)):
-    numero1 += 1
+while (numero1<(numero2 - 1)): #CONDIÇÃO PARA CONTAGEM
+    numero1 += 1 
     print (numero1)
 
 while (numero2 <(numero1 - 1)):
