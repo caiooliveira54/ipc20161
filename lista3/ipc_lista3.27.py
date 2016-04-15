8ae6db20745c7d3ee9e9526f1a5c77dcb55b61a3
nadinebrito <nadinecb2010@hotmail.com>
2016-04-15 13:46:43 -0400

    Update ipc_lista3.27.py

--- lista3/ipc_lista3.27.py
+++ lista3/ipc_lista3.27.py
@@ -5,6 +5,7 @@
 #Nickso Patrick Façanha Calheiros - 1615310059
 #Matheus Mota de Souza - 1615310016
 #Igor Menezes Sales Vieira - 1615310007
+#Nadine da Cunha Brito - 1615310040
 #
 q_turmas=int(input("informe a quantidade de turmas:\n"))
 cond=0
