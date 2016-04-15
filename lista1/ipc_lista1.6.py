18913e623571057760084031515d2f400b0b8743
Kid Mendes <kid.mon@hotmail.com>
2016-04-15 19:57:14 -0400

    Lista 1-Questão 6

---
+++ lista1/ipc_lista1.6.py
@@ -0,0 +1,18 @@
+#Introdução a Programação de Computadores
+#Professor: Jucimar Junior
+#Any Mendes Carvalho - 1615310044
+#Kid Mendes de Oliveira Neto - 1615310011
+#Victor Rafael da Silva e Silva - 1615310025
+#Eduardo Maia Freire - 1615310003
+#Luiz Alexandre Olivera de Souza-1615310057
+#Matheus Palheta Barbosa -1615310019
+#
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área")
+
+raio = 0  
+area = 0 
+
+raio = input("Entre com o valor do raio: ")  
+area = 3.14 * raio*raio 
+
+print ("Valor da area do circulo: %d m²" % area)
