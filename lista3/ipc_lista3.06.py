17f83b4369f421e50efcf5c87fa609f3fbdbd5bb
Shouryu12 <solelipecosta1@gmail.com>
2016-04-15 19:16:39 -0400

    Questão 6

---
+++ lista3/ipc_lista3.06.py
@@ -0,0 +1,24 @@
+#Introdução a programação de computadores;
+#Professor: Jucimar Junior
+#Felipe Henrique Bastos Costa - 1615310032
+#Lorene da Silva Marques - 1615310013
+#Caio de Oliveira Martins - 1615310031
+#Antonio Rodrigues de Souza Neto - 1615310028
+#Calebe Roberto Chaves da Silva Rebello - 1615310043
+
+numero = 1
+
+while (numero<=20):
+    print(numero)
+    numero += 1
+
+numeros = ""
+numero = 1
+
+print ("Mudando Direcao")
+
+while (numero<=20):
+    numeros += str(numero)+""
+    numero += 1
+
+print(numeros)
