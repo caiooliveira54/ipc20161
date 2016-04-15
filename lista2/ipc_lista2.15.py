7fef3c310b89b28081b0e0dcccee592846832023
AbFrota <beatrizffrotaa@gmail.com>
2016-04-15 15:49:33 -0300

    Update ipc_lista2.15.py

--- lista2/ipc_lista2.15.py
+++ lista2/ipc_lista2.15.py
@@ -1,9 +1,18 @@
-#Equipe 2
 #
-#Ana Beatriz Frota - 1615310027
+#introdução a programação de computadores
+#Professor: Jucimar JR
+#EQUIPE 2
+#
+#Ana Beatriz Frota  - 1615310027 
+#Kylciane Cristiny Lopes Freitas - 1615310052
+#Frankilin Yuri Gonçaves dos santos - 1615310033
+#Lucas Ferreira Soares - 1615310014
+#Luiz Gustavo Rocha Melo - 1615310015
 #Nahan Trindade Passos - 1615310021
+#Samuel Silva França - 1615310049
+#Luiz Alexandre Oliveira de Souza - 1615310057
 #
-#
+
 
 lado1 = float(input("Tamanho do primeiro lado:\n"))
 lado2 = float(input("Tamanho do segundo lado:\n"))
