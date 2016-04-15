cf17561ccca616968eed3f71d7287d9b3a774565
nadinebrito <nadinecb2010@hotmail.com>
2016-04-15 14:06:14 -0400

    Added files via upload

---
+++ lista3/ipc_lista3.35.py
@@ -0,0 +1,21 @@
+#Introduçao a Programaçao de Computadores
+#Professor: Jucimar Junior
+#Bruno de Oliveira Freire - 1615310030
+#Igor Menezes Sales Vieira - 1615310007
+#Matheus Mota de Souza - 1615310016
+#Nadine da Cunha Brito - 1615310040
+#Nickso Patrick Façanha Calheiros - 1615310059
+#Thiago Santos Borges - 1615310023
+#
+num = int(input("Informe até onde deseja analisar o codigo:\n"))
+i = 1
+x = 1
+
+while x <= num :
+    if x!=2 and x!=3 and x!=5 and x!=7:
+        if x/x == 1 and x/ 1 == x and x%2!=0 and x%3!=0 and x%4!=0 and x%5!=0 and x%6!=0 and x%7!=0 and x%8!=0 and x%9!=0 and x!=1:
+            print(x)
+    else:
+        print(x)
+    x+=1
+        
