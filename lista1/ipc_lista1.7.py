4724fc1e056d67a19a13a56b27f5131c9683eafe
Kid Mendes <kid.mon@hotmail.com>
2016-04-15 19:58:20 -0400

    Lista 1-Questão 7

---
+++ lista1/ipc_lista1.7.py
@@ -0,0 +1,17 @@
+#Introdução a Programação de Computadores
+#Professor: Jucimar Junior
+#Any Mendes Carvalho - 1615310044
+#Kid Mendes de Oliveira Neto - 1615310011
+#Victor Rafael da Silva e Silva - 1615310025
+#Eduardo Maia Freire - 1615310003
+#Luiz Alexandre Olivera de Souza-1615310057
+#Matheus Palheta Barbosa -1615310019
+#
+#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o #usuário
+   
+altura = input("Digite a altura do quadrado em metros: ")
+largura = input("Digite a largura do quadrado em metros: ")
+a = altura * largura
+print ("A area e: m²" , (a))
+print ("O DOBRO da area e: ",a * 2)
+
