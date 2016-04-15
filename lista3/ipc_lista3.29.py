ba1b462a03b9aae872eca02e90c301b70d62965c
caniiggia <caniiggia2010@hotmail.com>
2016-04-14 20:14:09 -0400

    Update ipc_lista3.29.py

--- lista3/ipc_lista3.29.py
+++ lista3/ipc_lista3.29.py
@@ -1,3 +1,10 @@
+#Introduçao a programaçao de computadores
+#Professor: Jucimar Junior
+#Bruno de Oliveira Freire - 1615310030
+#Thiago Santos Borges - 1615310023
+#Nickso Patrick Façanha Calheiros - 1615310059
+#Matheus Mota de Souza - 1615310016
+#Igor Menezes Sales Vieira - 16153100
 preco=1.99
 cont=1
 soma=0
