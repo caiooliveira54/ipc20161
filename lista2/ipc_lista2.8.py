345de97d691f5d29f78dca3ae702970174e19ebb
AbFrota <beatrizffrotaa@gmail.com>
2016-04-15 15:38:20 -0300

    Update ipc_lista2.8.py

--- lista2/ipc_lista2.8.py
+++ lista2/ipc_lista2.8.py
@@ -1,8 +1,18 @@
-#EQUIPE 2
-# Ana Beatriz Faria Frota - 1615310027
-# Nahan Trindade Passos - 1615310021
-# 
 #
+#introdução a programação de computadores
+#Professor: Jucimar JR
+#EQUIPE 2
+#
+#Ana Beatriz Frota  - 1615310027 
+#Kylciane Cristiny Lopes Freitas - 1615310052
+#Frankilin Yuri Gonçaves dos santos - 1615310033
+#Lucas Ferreira Soares - 1615310014
+#Luiz Gustavo Rocha Melo - 1615310015
+#Nahan Trindade Passos - 1615310021
+#Samuel Silva França - 1615310049
+#
+#
+
 
 p1 = float(input("informe o valor do primeiro produto: "))
 p2 = float(input("informe o valor do segundo produto: "))
