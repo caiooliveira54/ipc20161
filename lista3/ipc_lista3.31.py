ad99c28dd9a4f3fbb25e97b7949d0a6910e15f55
nadinebrito <nadinecb2010@hotmail.com>
2016-04-15 14:42:12 -0400

    Update ipc_lista3.31.py

--- lista3/ipc_lista3.31.py
+++ lista3/ipc_lista3.31.py
@@ -1,5 +1,5 @@
-#
-#ipc_lista_3_39-51(Questões 49 e 51 são iguais)
+#Introduçao a programaçao de computadores
+#Professor: Jucimar Junior
 #Thiago Santos Borges//Matricula->1615310023
 #Lorene da Silva Marques//Matricula->1615310013
 #Matheus Palheta Barbosa//Matricula->1615310019
