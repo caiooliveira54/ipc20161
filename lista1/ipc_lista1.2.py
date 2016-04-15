f58c36b714399378fccd2a7cc02f368f851e25be
any1m1c <anymendescarvalho@gmail.com>
2016-04-15 14:07:18 -0300

    Editando cabeçalho

--- lista1/ipc_lista1.2.py
+++ lista1/ipc_lista1.2.py
@@ -1,4 +1,11 @@
+#ipc_lista1.2
+#Professor: Jucimar Junior
 #Any Mendes Carvalho - 1615310044
+#
+#
+#
+#
+
 
 #Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
 
