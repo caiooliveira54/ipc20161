2f4326247969b3a446e6e263063d14570449cc45
arielrocha1 <ariel_rocha@live.com>
2016-04-15 18:25:57 -0400

    Update ipc_lista2.07.py

--- lista2/ipc_lista2.07.py
+++ lista2/ipc_lista2.07.py
@@ -11,6 +11,7 @@
 #Nahan Trindade Passos - 1615310021
 #Samuel Silva França - 1615310049
 #Luiz Alexandre Oliveira de Souza - 1615310057
+#Ariel Guilherme Rocha Capistrano - 1615310029
 #
 
 n1 = int(input("Insira um número: "))
