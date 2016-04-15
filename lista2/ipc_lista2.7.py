6ed3c705e8e89d1a69d7930271d6e1144d28722a
AbFrota <beatrizffrotaa@gmail.com>
2016-04-15 13:26:06 -0300

    Update ipc_lista2.7.py

--- lista2/ipc_lista2.7.py
+++ lista2/ipc_lista2.7.py
@@ -1,9 +1,9 @@
+#EQUIPE 2
+#
+#
 #
 # Ana Beatriz Faria Frota - 1615310027
-# Mateus Mota de Souza - 1615310016
-# Matheus Henrique Araujo Batista - 1615310039
 # Nahan Trindade Passos - 1615310021
-# Victor Hugo Souza Correia - 1615310024
 #
 
 n1 = int(input("Insira um número: "))
