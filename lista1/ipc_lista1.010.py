e5e2cc52a55aabcfbc37667faaa260f12a3dcba4
dudemaiaf <dudu_maia13@hotmail.com>
2016-04-15 19:55:36 -0400

    Update and rename ipc_lista1.10.py to ipc_lista1.010.py

---
+++ lista1/ipc_lista1.010.py
@@ -0,0 +1,17 @@
+#Introdução a programação de computadores
+#Professor: Jucimar Junior
+#Any Mendes Carvalho - 1615310044
+#Kid Mendes de Oliveira Neto - 1615310011
+#Victor Rafael da Silva e Silva - 1615310025
+#Eduardo Maia Freire - 1615310003
+#Luiz Alexandre Olivera de Souza-1615310057
+#Matheus Palheta Barbosa -1615310019
+#
+
+#Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Farenheit.
+
+c = input("Entre com a temperatura em graus Celsius : ")
+vt = c * 1.8
+f = vt + 32
+
+print ("A temperatura em Farenheit é: %.1f") % (f)
