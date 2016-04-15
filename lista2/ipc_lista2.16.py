bf9e10354109352f654b2e8f8e42a237c20cdc28
AbFrota <beatrizffrotaa@gmail.com>
2016-04-15 15:36:05 -0300

    Update ipc_lista2.16.py

--- lista2/ipc_lista2.16.py
+++ lista2/ipc_lista2.16.py
@@ -1,10 +1,15 @@
+#
+#introdução a programação de computadores
+#Professor: Jucimar JR
 #EQUIPE 2
+#
+#Ana Beatriz Frota  - 1615310027 
+#Kylciane Cristiny Lopes Freitas - 1615310052
+#Frankilin Yuri Gonçaves dos santos - 1615310033
+#Lucas Ferreira Soares - 1615310014
+#Luiz Gustavo Rocha Melo - 1615310015
 #Nahan Trindade Passos - 1615310021
-#Ana Beatriz Frota - 1615310027
-#
-#
-#
-#
+#Samuel Silva França - 1615310049
 #
 #
 
