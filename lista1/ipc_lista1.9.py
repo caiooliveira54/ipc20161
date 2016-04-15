ef1e2614c8c297fed33e30c35bebe3e959c8d217
dudemaiaf <dudu_maia13@hotmail.com>
2016-04-15 19:52:41 -0400

    Added files via upload

---
+++ lista1/ipc_lista1.9.py
@@ -0,0 +1,16 @@
+#Introdução a programação de computadores
+#Professor: Jucimar Junior
+#Any Mendes Carvalho - 1615310044
+#Kid Mendes de Oliveira Neto - 1615310011
+#Victor Rafael da Silva e Silva - 1615310025
+#Eduardo Maia Freire - 1615310003
+#Luiz Alexandre Olivera de Souza-1615310057
+#Matheus Palheta Barbosa -1615310019
+#
+#Fa�a um Programa que pe�a a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+
+F = input('Entre com a temperatura em graus Farenheit: ')  
+  
+C = ((F-32) / 1.8)  
+  
+print "A temperatura em Celcius e': %.1f" % (C) 
