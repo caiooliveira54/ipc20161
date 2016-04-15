12d439373fe0bf2a00a2036f826f9d64ec34c153
any1m1c <anymendescarvalho@gmail.com>
2016-04-15 13:44:13 -0300

    Create ipc_lista1.2.py

---
+++ lista1/ipc_lista1.2.py
@@ -0,0 +1,6 @@
+#Any Mendes Carvalho - 1615310044
+
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+number = input("Digite um número: ")
+print "O número digitado foi ",number
