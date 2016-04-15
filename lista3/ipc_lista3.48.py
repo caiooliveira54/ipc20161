4c1de08e05ec634c919d548c9c20be07bd386345
nadinebrito <nadinecb2010@hotmail.com>
2016-04-15 14:56:50 -0400

    Update ipc_lista3.48.py

--- lista3/ipc_lista3.48.py
+++ lista3/ipc_lista3.48.py
@@ -5,7 +5,7 @@
 #Nickso Patrick Façanha Calheiros - 1615310059
 #Mateus Mota de Souza - 1615310016
 #Igor Menezes Sales Vieira - 1615310007
-#
+#Nadine da Cunha Brito - 1615310040
 numero = int(input("Digite um numero:"))
 aux = numero
 reverso = 0
