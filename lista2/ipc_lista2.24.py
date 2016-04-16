bfc27d75ce25379f6395049786de26157cbf56a1
arielrocha1 <ariel_rocha@live.com>
2016-04-15 20:13:41 -0400

    Added files via upload

---
+++ lista2/ipc_lista2.24.py
@@ -0,0 +1,67 @@
+numero1 = float(input("Digite o primeiro numero:"))
+numero2 = float(input("Digite o segundo numero:"))
+
+operacao = raw_input("Escolha uma operacao: (+)-Adicao;(-)-Subtracao;(*)-Multiplicacao;(/)-Divisao\n")
+
+if(operacao == "+"):
+    adicao = (numero1+numero2)
+    print("O resultado sera:",adicao)
+    if(adicao%2 == 0):
+        print("Numero par")
+    else:
+        print("Numero impar")
+    if(adicao>0):
+        print("Positivo")
+    else:
+        print("Negativo")
+    if(round(adicao) != adicao):
+        print("Decimal")
+    else:
+        print("Inteiro")
+if(operacao == "-"):
+    subtracao = (numero1-numero2)
+    print("O resultado sera:",subtracao)
+    if(subtracao%2 == 0):
+        print("Numero par")
+    else:
+        print("Numero impar")
+    if(subtracao>0):
+        print("Positivo")
+    else:
+        print("Negativo")
+    if(round(subtracao) != subtracao):
+        print("Decimal")
+    else:
+        print("Inteiro")
+if(operacao == "*"):
+    multiplicacao = (numero1*numero2)
+    print("O resultado sera:",multiplicacao)
+    if(multiplicacao%2 == 0):
+        print("Numero par")
+    else:
+        print("Numero impar")
+    if(multiplicacao>0):
+        print("Positivo")
+    else:
+        print("Negativo")
+    if(round(multiplicacao) != multiplicacao):
+        print("Decimal")
+    else:
+        print("Inteiro")
+if(operacao == "/"):
+    divisao = (numero1/numero2)
+    print("O resultado sera:",divisao)
+    if(divisao%2 == 0):
+        print("Numero par")
+    else:
+        print("Numero impar")
+    if(divisao>0):
+        print("Positivo")
+    else:
+        print("Negativo")
+    if(round(divisao) != divisao):
+        print("Decimal")
+    else:
+        print("Inteiro")
+
+
