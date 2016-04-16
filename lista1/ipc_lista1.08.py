c92271a5fad28cf571b29fc645858b91a0cd98a2
dudemaiaf <dudu_maia13@hotmail.com>
2016-04-15 20:13:02 -0400

    Update ipc_lista1.08.py

--- lista1/ipc_lista1.08.py
+++ lista1/ipc_lista1.08.py
@@ -4,7 +4,7 @@
 #Kid Mendes de Oliveira Neto - 1615310011
 #Victor Rafael da Silva e Silva - 1615310025
 #Eduardo Maia Freire - 1615310003
-#Luiz Alexandre Olivera de Souza-1615310057
+#Luiz Gustavo de Rocha Melo - 1615310015
 #Matheus Palheta Barbosa -1615310019
 #
 #Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
