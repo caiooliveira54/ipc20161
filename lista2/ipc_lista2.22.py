31c0a73c0178c97ceb27a2b336c95b9db92b8c22
arielrocha1 <ariel_rocha@live.com>
2016-04-15 20:14:53 -0400

    Update ipc_lista2.22.py

--- lista2/ipc_lista2.22.py
+++ lista2/ipc_lista2.22.py
@@ -1,3 +1,19 @@
+#
+#introdução a programação de computadores
+#Professor: Jucimar JR
+#EQUIPE 2
+#
+#Ana Beatriz Frota  - 1615310027 
+#Kylciane Cristiny Lopes Freitas - 1615310052
+#Franklin Yuri Gonçaves dos Santos - 1615310033
+#Lucas Ferreira Soares - 1615310014
+#Luiz Gustavo Rocha Melo - 1615310015
+#Nahan Trindade Passos - 1615310021
+#Samuel Silva França - 1615310049
+#Luiz Alexandre Oliveira de Souza - 1615310057
+#Ariel Guilherme Rocha Capistrano - 1615310029
+#
+
 numero = int(input("Digite um numero inteiro:"))
 
 if(numero%2 == 0):
