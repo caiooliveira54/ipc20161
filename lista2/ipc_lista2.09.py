ab863a4cf1dc50cfd1b9b62c74157a3761bc7e5a
arielrocha1 <ariel_rocha@live.com>
2016-04-15 20:05:41 -0400

    Update ipc_lista2.09.py

--- lista2/ipc_lista2.09.py
+++ lista2/ipc_lista2.09.py
@@ -11,9 +11,8 @@
 #Nahan Trindade Passos - 1615310021
 #Samuel Silva França - 1615310049
 #Luiz Alexandre Oliveira de Souza - 1615310057
+#Ariel Guilherme Rocha Capistrano - 1615310029
 #
-
-
 
 cont = 1 #contador
 numero = 1 #variável 
