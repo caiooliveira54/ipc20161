cce25b79f089c0334a8d1784585a5735b27e3349
dudemaiaf <dudu_maia13@hotmail.com>
2016-04-15 20:00:41 -0400

    Update ipc_lista1.12.py

--- lista1/ipc_lista1.12.py
+++ lista1/ipc_lista1.12.py
@@ -1,9 +1,12 @@
-#Alexander Emerson Batalha Carlos - 1615310051
+#Introdução a programação de computadores
+#Professor: Jucimar Junior
 #Any Mendes Carvalho - 1615310044
-#Ariel Guilherme Rocha Capistrano - 1615310029
-#Beatriz Pessoa Longato - 1615310001
-#Nickso Patrick Fa�anha Calheiros - 1615310059
-
+#Kid Mendes de Oliveira Neto - 1615310011
+#Victor Rafael da Silva e Silva - 1615310025
+#Eduardo Maia Freire - 1615310003
+#Luiz Alexandre Olivera de Souza-1615310057
+#Matheus Palheta Barbosa -1615310019
+#
 #Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte formula: \n (72.7*altura) - 58
 
 altura  = input("Digite sua altura em metros, separa por ponto (ex: 1.60): ")
@@ -11,4 +14,4 @@ altura  = input("Digite sua altura em metros, separa por ponto (ex: 1.60): ")
 vT = 72.7 * altura 
 r = vT - 58
 
-print "Seu peso ideal e' ",r 
+print ("Seu peso ideal e' ",r) 
