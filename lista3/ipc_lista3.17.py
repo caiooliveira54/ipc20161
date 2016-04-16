be0801de2a789c3040de10ccc3dd7de32f54bb1f
caleberebello <calebe_rebello@hotmail.com>
2016-04-15 20:22:40 -0400

    Subindo questão 17

---
+++ lista3/ipc_lista3.17.py
@@ -0,0 +1,18 @@
+#Introdução a programação de computadores;
+#Professor: Jucimar Junior
+#Felipe Henrique Bastos Costa - 1615310032
+#Lorene da Silva Marques - 1615310013
+#Caio de Oliveira Martins - 1615310031
+#Antonio Rodrigues de Souza Neto - 1615310028
+#Calebe Roberto Chaves da Silva Rebello - 1615310043
+
+n = int(input("Digite o valor de n: "))
+
+produto = n
+contador =  n-1
+
+while contador > 1:
+    produto = produto*contador
+    contador = contador - 1
+
+print(n, "!=", produto)
