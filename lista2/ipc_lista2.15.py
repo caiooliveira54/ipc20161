c0dae201ad24079fd0fb0d046a1a803d2421ce41
arielrocha1 <ariel_rocha@live.com>
2016-04-15 20:03:48 -0400

    Update ipc_lista2.15.py

--- lista2/ipc_lista2.15.py
+++ lista2/ipc_lista2.15.py
@@ -11,8 +11,8 @@
 #Nahan Trindade Passos - 1615310021
 #Samuel Silva França - 1615310049
 #Luiz Alexandre Oliveira de Souza - 1615310057
+#Ariel Guilherme Rocha Capistrano - 1615310029
 #
-
 
 lado1 = float(input("Tamanho do primeiro lado:\n"))
 lado2 = float(input("Tamanho do segundo lado:\n"))
