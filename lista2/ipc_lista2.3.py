4b7cefc11e5a1a3af0cd115f528478c549060320
dudemaiaf <dudu_maia13@hotmail.com>
2016-04-17 01:34:37 -0400

    Added files via upload

---
+++ lista2/ipc_lista2.3.py
@@ -0,0 +1,21 @@
+#introdução a programação de computadores
+#Professor: Jucimar JR
+#EQUIPE 1
+#
+#Any Mendes Carvalho - 1615310044
+#Kid Mendes de Oliveira Neto - 1615310011
+#Victor Rafael da Silva e Silva - 1615310025
+#Eduardo Maia Freire - 1615310003
+#Luiz Gustavo de Rocha Melo - 1615310015
+#Matheus Palheta Barbosa -1615310019
+
+sexo = input("Informe seu sexo F para Femenino e M para Masculino: ")
+
+if (sexo == "M"):
+    print ("Sexo Masculino.")
+else:
+    if (sexo == "F"):
+        print ("Sexo Femenino.")
+    else:
+        print ("Sexo Invalido.")
+            
