df54ddcb6997dc39ab9410d9a5359dbc561398cb
dudemaiaf <dudu_maia13@hotmail.com>
2016-04-17 01:35:36 -0400

    Added files via upload

---
+++ lista2/ipc_lista2.4.py
@@ -0,0 +1,17 @@
+#introdução a programação de computadores
+#Professor: Jucimar JR
+#EQUIPE 1
+#
+#Any Mendes Carvalho - 1615310044
+#Kid Mendes de Oliveira Neto - 1615310011
+#Victor Rafael da Silva e Silva - 1615310025
+#Eduardo Maia Freire - 1615310003
+#Luiz Gustavo de Rocha Melo - 1615310015
+#Matheus Palheta Barbosa -1615310019
+
+letra = input("Informe uma letra: ")
+
+if (letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u"):
+    print (letra, "e uma vogal")
+else:
+    print(letra,"e uma consoante")
