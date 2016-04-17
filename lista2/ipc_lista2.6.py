46429febadf4f0986ee03d4bd84d2cf680192f08
dudemaiaf <dudu_maia13@hotmail.com>
2016-04-17 01:37:03 -0400

    Added files via upload

---
+++ lista2/ipc_lista2.6.py
@@ -0,0 +1,24 @@
+#introdu��o a programa��o de computadores
+#Professor: Jucimar JR
+#EQUIPE 1
+#
+#Any Mendes Carvalho - 1615310044
+#Kid Mendes de Oliveira Neto - 1615310011
+#Victor Rafael da Silva e Silva - 1615310025
+#Eduardo Maia Freire - 1615310003
+#Luiz Gustavo de Rocha Melo - 1615310015
+#Matheus Palheta Barbosa -1615310019
+
+n1 = int(input("Insira um número: "))
+n2 = int(input("Insira outro número: "))
+n3 = int(input("Insira mais um número: "))
+
+if n1>n2:
+    if n1>n3:
+        print ("O primeiro número é maior")
+if n2>n1:
+    if n2>n3:
+        print ("O segundo número é maior")
+if n3>n1:
+    if n3>n2:
+        print ("O terceiro número é maior")
