44c32d688e95d4a490cd2422fca2fac68c14448c
arielrocha1 <ariel_rocha@live.com>
2016-04-18 03:15:41 -0400

    Update ipc_lista2.03.py

--- lista2/ipc_lista2.03.py
+++ lista2/ipc_lista2.03.py
@@ -4,19 +4,20 @@
 #EQUIPE 1
 #
 #Any Mendes Carvalho - 1615310044
-#Kid Mendes de Oliveira Neto - 1615310011
-#Victor Rafael da Silva e Silva - 1615310025
 #Eduardo Maia Freire - 1615310003
+#Kid Mendes de Oliveira Neto - 1615310011
 #Luiz Gustavo de Rocha Melo - 1615310015
 #Matheus Palheta Barbosa -1615310019
+#Victor Rafael da Silva e Silva - 1615310025
 #
 
-sexo = raw_input("Informe seu sexo F para Femenino e M para Masculino: ")
+sexo = input("Informe seu sexo F para Feminino e M para Masculino: ")
 
 if (sexo == "M"):
     print ("Sexo Masculino.")
+
+elif(sexo == "F"):
+    print ("Sexo Feminino.")
+
 else:
-    if (sexo == "F"):
-        print ("Sexo Femenino.")
-    else:
-        print ("Sexo Invalido.")
+    print ("Sexo Invalido.")
