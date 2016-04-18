93abb68a847acbdc0aa73d046701be6d2fd7c4d4
Mateus Souza <mota.mateus@live.com>
2016-04-18 11:40:02 -0400

    Update ipc_lista1.11.py

--- lista1/ipc_lista1.11.py
+++ lista1/ipc_lista1.11.py
@@ -11,14 +11,14 @@
 #Victor Rafael da Silva e Silva - 1615310025
 #
   
-number1 = int(input("Digite o primeiro numero inteiro:  "))
-number2 = int(input("Digite o segundo numero inteiro:  "))
-nReal = float(input("Digite o numero real:  "))
+number1 = int(input("Digite o primeiro numero inteiro:  ")) # Solicita um número inteiro ao usuário
+number2 = int(input("Digite o segundo numero inteiro:  ")) # A mesma coisa que a anterior
+nReal = float(input("Digite o numero real:  ")) # Aqui é solicitado um número real
 
-resultado1 = (number1 * 2) * (number2 / 2)          
-resultado2 = (number1 * 3) + nReal
-resultado3 = nReal ** 3
+resultado1 = (number1 * 2) * (number2 / 2) # Calcula o produto do dobro do primeiro número com a metade do segundo
+resultado2 = (number1 * 3) + nReal # Calcula a soma do triplo do primeiro com o número real
+resultado3 = nReal ** 3 # Aqui calcula o cubo do número real
 
-print ("o produto do dobro do primeiro com metade do segundo = ",resultado1)   
-print ("a soma do triplo do primeiro com o terceiro = ",resultado2)
-print ("o terceiro elevado ao cubo",resultado3)
+print ("o produto do dobro do primeiro com metade do segundo = ",resultado1) # Impressão do produto do dobro do primeiro número com a metade do segundo
+print ("a soma do triplo do primeiro com o terceiro = ",resultado2) # Impressão da soma do triplo do primeiro com o número real
+print ("o terceiro elevado ao cubo",resultado3) # Impressão o cubo do número real
