8df94f9474fe3a6a5e3b0a8ee29cfa02ad86e635
arielrocha1 <ariel_rocha@live.com>
2016-04-18 02:55:10 -0400

    Update ipc_lista1.10.py

--- lista1/ipc_lista1.10.py
+++ lista1/ipc_lista1.10.py
@@ -1,14 +1,15 @@
-#Introdução a programação de computadores
-#Professor: Jucimar Junior
+#
+#introdução a programação de computadores
+#Professor: Jucimar JR
+#EQUIPE 1
+#
 #Any Mendes Carvalho - 1615310044
-#Kid Mendes de Oliveira Neto - 1615310011
-#Victor Rafael da Silva e Silva - 1615310025
 #Eduardo Maia Freire - 1615310003
+#Kid Mendes de Oliveira Neto - 1615310011
 #Luiz Gustavo de Rocha Melo - 1615310015
 #Matheus Palheta Barbosa -1615310019
+#Victor Rafael da Silva e Silva - 1615310025
 #
-
-#Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Farenheit.
 
 c = input("Entre com a temperatura em graus Celsius : ")
 vt = c * 1.8
