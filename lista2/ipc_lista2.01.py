b99eee5e936d2b61610e34618b6e2a0a7484589d
arielrocha1 <ariel_rocha@live.com>
2016-04-18 03:11:05 -0400

    Update ipc_lista2.01.py

--- lista2/ipc_lista2.01.py
+++ lista2/ipc_lista2.01.py
@@ -1,26 +1,24 @@
 #
-#Introdução a programação de computadores
+#introdução a programação de computadores
 #Professor: Jucimar JR
 #EQUIPE 1
 #
 #Any Mendes Carvalho - 1615310044
-#Kid Mendes de Oliveira Neto - 1615310011
-#Victor Rafael da Silva e Silva - 1615310025
 #Eduardo Maia Freire - 1615310003
+#Kid Mendes de Oliveira Neto - 1615310011
 #Luiz Gustavo de Rocha Melo - 1615310015
 #Matheus Palheta Barbosa -1615310019
+#Victor Rafael da Silva e Silva - 1615310025
 #
-#Faça um Programa que peça dois números e imprima o maior deles. 
-#
-
-
 
 num1 = float(input("Informe um numero: "))
 num2 = float(input("Informe outro numero: "))
 
 if (num1 > num2):
     print ("O maoir numero e: ",num1)
+
 elif(num1 == num2):
     print("Os dois numeros sao iguais.")
+
 else:
     print ("O maior numero e: ",num2)
