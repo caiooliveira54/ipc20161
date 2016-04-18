24e3e73f61727b4be8a7343ee341403aacf4d5f2
arielrocha1 <ariel_rocha@live.com>
2016-04-18 02:58:46 -0400

    Update ipc_lista1.16.py

--- lista1/ipc_lista1.16.py
+++ lista1/ipc_lista1.16.py
@@ -1,16 +1,14 @@
 #
-#introdução a programação de computadores
+#introduÃ§Ã£o a programaÃ§Ã£o de computadores
 #Professor: Jucimar JR
 #EQUIPE 1
 #
 #Any Mendes Carvalho - 1615310044
-#Kid Mendes de Oliveira Neto - 1615310011
-#Victor Rafael da Silva e Silva - 1615310025
 #Eduardo Maia Freire - 1615310003
+#Kid Mendes de Oliveira Neto - 1615310011
 #Luiz Gustavo de Rocha Melo - 1615310015
 #Matheus Palheta Barbosa -1615310019
-#
-#Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00. Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total.
+#Victor Rafael da Silva e Silva - 1615310025
 #
 
 metros = int(input("Digite a area para ser pintada: "))
