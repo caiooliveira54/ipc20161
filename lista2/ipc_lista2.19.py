57467f3809c51d861e2f4ff797583e0248af745d
arielrocha1 <ariel_rocha@live.com>
2016-04-18 03:29:33 -0400

    Update ipc_lista2.19.py

--- lista2/ipc_lista2.19.py
+++ lista2/ipc_lista2.19.py
@@ -4,14 +4,14 @@
 #EQUIPE 2
 #
 #Ana Beatriz Frota  - 1615310027 
+#Ariel Guilherme Rocha Capistrano - 1615310029
+#Frankilin Yuri Gonçaves dos santos - 1615310033
 #Kylciane Cristiny Lopes Freitas - 1615310052
-#Franklin Yuri Gonçaves dos Santos - 1615310033
 #Lucas Ferreira Soares - 1615310014
+#Luiz Alexandre Oliveira de Souza - 1615310057
 #Luiz Gustavo Rocha Melo - 1615310015
 #Nahan Trindade Passos - 1615310021
 #Samuel Silva França - 1615310049
-#Luiz Alexandre Oliveira de Souza - 1615310057
-#Ariel Guilherme Rocha Capistrano - 1615310029
 #
 
 numero = int(input("Digite um numero menor que 1000:\n"))
@@ -23,10 +23,12 @@ if(quantidade_numero == 3):
     dezenas = numerostr[1:2]
     unidade = numerostr[2:3]
     print(numerostr+"= "+centenas+" centenas ,"+dezenas+" dezenas e "+unidade+" unidades")
+
 if(quantidade_numero == 2):
     dezenas = numerostr[0:1]
     unidade = numerostr[1:2]
     print(numerostr+"= "+dezenas+" dezenas e "+unidade+" unidades")
+
 if(quantidade_numero == 1):
     unidade = numerostr[0:1]
     print(numerostr+"= "+unidade+" unidades")
