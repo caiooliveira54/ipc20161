766519787eebd407359a3225cd09e9497fce0eb0
arielrocha1 <ariel_rocha@live.com>
2016-04-18 02:51:42 -0400

    Update ipc_lista1.03.py

--- lista1/ipc_lista1.03.py
+++ lista1/ipc_lista1.03.py
@@ -4,11 +4,11 @@
 #EQUIPE 1
 #
 #Any Mendes Carvalho - 1615310044
-#Kid Mendes de Oliveira Neto - 1615310011
-#Victor Rafael da Silva e Silva - 1615310025
 #Eduardo Maia Freire - 1615310003
+#Kid Mendes de Oliveira Neto - 1615310011
 #Luiz Gustavo de Rocha Melo - 1615310015
 #Matheus Palheta Barbosa -1615310019
+#Victor Rafael da Silva e Silva - 1615310025
 #
 
 number1 = input("Digite o primeiro: ")
