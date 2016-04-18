55638a08730194e6a51a38cf0598804e3d8be9b5
arielrocha1 <ariel_rocha@live.com>
2016-04-18 03:18:06 -0400

    Update ipc_lista2.05.py

--- lista2/ipc_lista2.05.py
+++ lista2/ipc_lista2.05.py
@@ -4,16 +4,18 @@
 #EQUIPE 1
 #
 #Any Mendes Carvalho - 1615310044
-#Kid Mendes de Oliveira Neto - 1615310011
-#Victor Rafael da Silva e Silva - 1615310025
 #Eduardo Maia Freire - 1615310003
+#Kid Mendes de Oliveira Neto - 1615310011
 #Luiz Gustavo de Rocha Melo - 1615310015
 #Matheus Palheta Barbosa -1615310019
+#Victor Rafael da Silva e Silva - 1615310025
 #
 
 n1 = int(input("Insira a primeira nota: "))
 n2 = int(input("Insira a segunda nota: "))
+
 media = (n1+n2)/2
+
 if media == 10:
     print ("Aprovado com distinÃ§Ã£o")
 elif media >= 7:
