211312e99f2f679c54ce82097e59d162c50f56a6
arielrocha1 <ariel_rocha@live.com>
2016-04-18 02:53:17 -0400

    Update ipc_lista1.06.py

--- lista1/ipc_lista1.06.py
+++ lista1/ipc_lista1.06.py
@@ -4,14 +4,15 @@
 #EQUIPE 1
 #
 #Any Mendes Carvalho - 1615310044
-#Kid Mendes de Oliveira Neto - 1615310011
-#Victor Rafael da Silva e Silva - 1615310025
 #Eduardo Maia Freire - 1615310003
+#Kid Mendes de Oliveira Neto - 1615310011
 #Luiz Gustavo de Rocha Melo - 1615310015
 #Matheus Palheta Barbosa -1615310019
+#Victor Rafael da Silva e Silva - 1615310025
 #
 
 raio = float(input("Entre com o valor do raio: "))  
+
 area = 3.14 * raio*raio 
 
 print ("Valor da area do circulo: %d m²" % area)
