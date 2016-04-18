f8e1f4cd5bdc0d1e99b3da173c028da2ffd6477e
arielrocha1 <ariel_rocha@live.com>
2016-04-18 02:38:10 -0400

    Update ipc_lista1.06.py

--- lista1/ipc_lista1.06.py
+++ lista1/ipc_lista1.06.py
@@ -12,10 +12,7 @@
 #
 #Faça um Programa que peça o raio de um círculo, calcule e mostre sua área")
 
-raio = 0  
-area = 0 
-
-raio = input("Entre com o valor do raio: ")  
+raio = float(input("Entre com o valor do raio: "))  
 area = 3.14 * raio*raio 
 
 print ("Valor da area do circulo: %d m²" % area)
