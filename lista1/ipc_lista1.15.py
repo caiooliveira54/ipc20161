461d11c98f5a05addb61a3d2b7c218d13287e397
Mateus Souza <mota.mateus@live.com>
2016-04-18 12:06:12 -0400

    Update ipc_lista1.15.py

--- lista1/ipc_lista1.15.py
+++ lista1/ipc_lista1.15.py
@@ -11,15 +11,15 @@
 #Victor Rafael da Silva e Silva - 1615310025
 #
 
-print ("Vamos calcular quanto voce ganha num mes?")
-ganhos_horas = float(input("Insira quanto voce ganha por hora: "))
-horas_trabalhadas = int(input("Insira a quantidade de horas trabalhadas: "))
+print ("Vamos calcular quanto voce ganha num mes?") # Impressão de mensagem ao usuário
+ganhos_horas = float(input("Insira quanto voce ganha por hora: ")) # Solicitará os ganhos por hora 
+horas_trabalhadas = int(input("Insira a quantidade de horas trabalhadas: ")) # Solicitará a quantidade de horas trabalhadas
 
-salario = ganhos_horas * horas_trabalhadas
-imposto_r = salario * 0.11
-inss = salario * 0.08
-sindicato = salario * 0.05
-descontos = imposto_r + inss + sindicato
-salario_liquido = salario - descontos
+salario = ganhos_horas * horas_trabalhadas # Calculará o salário
+imposto_r = salario * 0.11 # Calculará o destinado ao imposto de renda
+inss = salario * 0.08 # Calculará o destinado ao INSS
+sindicato = salario * 0.05 # Calculará o destinado ao sindicato
+descontos = imposto_r + inss + sindicato # Os descontos do salário final
+salario_liquido = salario - descontos # O valor a ser recebido
 
-print ("Seu salario bruto e de R$%.2f, serao descontados R$%.2f para imposto de renda, R$%.2f para o INSS e R$%.2f para o sindicato.\nSeu salario liquido e de R$%.2f"%(salario, imposto_r, inss, sindicato, salario_liquido))
+print ("Seu salario bruto e de R$%.2f, serao descontados R$%.2f para imposto de renda, R$%.2f para o INSS e R$%.2f para o sindicato.\nSeu salario liquido e de R$%.2f"%(salario, imposto_r, inss, sindicato, salario_liquido)) # Impressão de todos os valores calculados na etapa de processamento
