11ea5e9096e067871a47a2991f2b0deed7a19f1b
arielrocha1 <ariel_rocha@live.com>
2016-04-18 02:58:11 -0400

    Update ipc_lista1.15.py

--- lista1/ipc_lista1.15.py
+++ lista1/ipc_lista1.15.py
@@ -1,27 +1,14 @@
 #
-#introdução a programação de computadores
+#introduÃ§Ã£o a programaÃ§Ã£o de computadores
 #Professor: Jucimar JR
 #EQUIPE 1
 #
 #Any Mendes Carvalho - 1615310044
-#Kid Mendes de Oliveira Neto - 1615310011
-#Victor Rafael da Silva e Silva - 1615310025
 #Eduardo Maia Freire - 1615310003
+#Kid Mendes de Oliveira Neto - 1615310011
 #Luiz Gustavo de Rocha Melo - 1615310015
 #Matheus Palheta Barbosa -1615310019
-#
-#Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês, sabendo-se que são descontados 11% para o Imposto de Renda, 8% para o INSS e 5% para o sindicato, faça um programa que nos dê:
-# salário bruto.
-# quanto pagou ao INSS.
-# quanto pagou ao sindicato.
-# o salário líquido.
-# calcule os descontos e o salário líquido, conforme a tabela abaixo:
-#  + Salário Bruto : R$
-#  - IR (11%) : R$
-#  - INSS (8%) : R$
-#  - Sindicato ( 5%) : R$
-#  = Salário Liquido : R$
-#   Obs.: Salário Bruto - Descontos = Salário Líquido.
+#Victor Rafael da Silva e Silva - 1615310025
 #
 
 print ("Vamos calcular quanto voce ganha num mes?")
