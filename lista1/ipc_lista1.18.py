b760d6531ef65351e7dbb26cdbf02040543cd83c
arielrocha1 <ariel_rocha@live.com>
2016-04-18 02:59:42 -0400

    Update ipc_lista1.18.py

--- lista1/ipc_lista1.18.py
+++ lista1/ipc_lista1.18.py
@@ -1,18 +1,15 @@
 #
-#Introdução a programação de computadores
+#introdução a programação de computadores
 #Professor: Jucimar JR
 #EQUIPE 1
 #
 #Any Mendes Carvalho - 1615310044
-#Kid Mendes de Oliveira Neto - 1615310011
-#Victor Rafael da Silva e Silva - 1615310025
 #Eduardo Maia Freire - 1615310003
+#Kid Mendes de Oliveira Neto - 1615310011
 #Luiz Gustavo de Rocha Melo - 1615310015
 #Matheus Palheta Barbosa -1615310019
+#Victor Rafael da Silva e Silva - 1615310025
 #
-#Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos)
-#
-
 
 tamanho = float(input("Tamanho do arquivo em mb:\n"))
 velocidade = float(input("Velocidade do link:\n"))
