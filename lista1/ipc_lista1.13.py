f1cb8f471e4f0d2280e8436c2d7cbd497b6de72c
arielrocha1 <ariel_rocha@live.com>
2016-04-18 02:57:04 -0400

    Update ipc_lista1.13.py

--- lista1/ipc_lista1.13.py
+++ lista1/ipc_lista1.13.py
@@ -1,19 +1,15 @@
 #
-#introdução a programação de computadores
+#introduÃ§Ã£o a programaÃ§Ã£o de computadores
 #Professor: Jucimar JR
 #EQUIPE 1
 #
 #Any Mendes Carvalho - 1615310044
-#Kid Mendes de Oliveira Neto - 1615310011
-#Victor Rafael da Silva e Silva - 1615310025
 #Eduardo Maia Freire - 1615310003
+#Kid Mendes de Oliveira Neto - 1615310011
 #Luiz Gustavo de Rocha Melo - 1615310015
 #Matheus Palheta Barbosa -1615310019
+#Victor Rafael da Silva e Silva - 1615310025
 #
-#Tendo como dados de entrada a altura e o sexo de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
-#Para homens: (72.7*h) - 58
-#Para mulheres: (62.1*h) - 44.7 (h = altura)
-#Peça o peso da pessoa e informe se ela está dentro, acima ou abaixo do peso.
 
 altura = float(input("Entre com a sua altura: "))
 sexo = input("Sexo M ou F: ")
