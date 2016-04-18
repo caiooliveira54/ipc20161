a384cad03db6055a2ca60dbfe0df2ce438e17494
arielrocha1 <ariel_rocha@live.com>
2016-04-18 02:52:46 -0400

    Update ipc_lista1.05.py

--- lista1/ipc_lista1.05.py
+++ lista1/ipc_lista1.05.py
@@ -4,13 +4,15 @@
 #EQUIPE 1
 #
 #Any Mendes Carvalho - 1615310044
-#Kid Mendes de Oliveira Neto - 1615310011
-#Victor Rafael da Silva e Silva - 1615310025
 #Eduardo Maia Freire - 1615310003
+#Kid Mendes de Oliveira Neto - 1615310011
 #Luiz Gustavo de Rocha Melo - 1615310015
 #Matheus Palheta Barbosa -1615310019
+#Victor Rafael da Silva e Silva - 1615310025
 #
 
 metros = float(input("Digite o valor em metros que deseja converter em centimetros: "))
+
 centimetros = metros * 100 
-print ("Esse valor equivale a: %d" %centimetros,"cm")
+
+rint ("Esse valor equivale a: %d" %centimetros,"cm")
