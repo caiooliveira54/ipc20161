09347d62953173d5f67f5db520564127b5da15b4
arielrocha1 <ariel_rocha@live.com>
2016-04-18 02:47:18 -0400

    Update ipc_lista1.08.py

--- lista1/ipc_lista1.08.py
+++ lista1/ipc_lista1.08.py
@@ -10,11 +10,10 @@
 #Luiz Gustavo de Rocha Melo - 1615310015
 #Matheus Palheta Barbosa -1615310019
 #
-#Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
 
-QntHora = input("Entre com o valor de seu rendimento por hora: ")  
-hT = input("Entre com a quantidade de horas trabalhadas no mês: ")  
+QntHora = float(input("Entre com o valor de seu rendimento por hora: "))  
+hT = float(input("Entre com a quantidade de horas trabalhadas no mês: "))
   
 Salario = round(QntHora*hT,2) 
 
-print (("\n Voce ganhou %.2f reais neste mes") % (Salario))
+print ("/nVoce ganhou %.2f reais neste mes" %Salario)
