0ab0984ddc170151f122bc17f7058c13b39b0991
Mateus Souza <mota.mateus@live.com>
2016-04-18 11:19:37 -0400

    Update ipc_lista1.02.py

--- lista1/ipc_lista1.02.py
+++ lista1/ipc_lista1.02.py
@@ -11,6 +11,6 @@
 #Victor Rafael da Silva e Silva - 1615310025
 #
 
-number = input("Digite um número: ")
+number = input("Digite um número: ") # Aqui é solicitado um número qualquer ao usuário
 
-print ("O número digitado foi ",number)
+print ("O número digitado foi ",number) # Logo após digitado o número é impresso
