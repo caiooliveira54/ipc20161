2dcf56be4817488750e1e62bb134f3e09576d0a0
arielrocha1 <ariel_rocha@live.com>
2016-04-18 02:57:31 -0400

    Update ipc_lista1.14.py

--- lista1/ipc_lista1.14.py
+++ lista1/ipc_lista1.14.py
@@ -1,16 +1,14 @@
 #
-#introdução a programação de computadores
+#introduÃ§Ã£o a programaÃ§Ã£o de computadores
 #Professor: Jucimar JR
 #EQUIPE 1
 #
 #Any Mendes Carvalho - 1615310044
-#Kid Mendes de Oliveira Neto - 1615310011
-#Victor Rafael da Silva e Silva - 1615310025
 #Eduardo Maia Freire - 1615310003
+#Kid Mendes de Oliveira Neto - 1615310011
 #Luiz Gustavo de Rocha Melo - 1615310015
 #Matheus Palheta Barbosa -1615310019
-#
-#João Papo-de-Pescador, homem de bem, comprou um microcomputador para controlar o rendimento diário de seu trabalho. Toda vez que ele traz um peso de peixes maior que o estabelecido pelo regulamento de pesca do estado de São Paulo (50 quilos) deve pagar uma multa de R$ 4,00 por quilo excedente. João precisa que você faça um programa que leia a variável peso (peso de peixes) e verifique se há excesso. Se houver, gravar na variável excesso e na variável multa o valor da multa que João deverá pagar. Caso contrário mostrar tais variáveis com o conteúdo ZERO.
+#Victor Rafael da Silva e Silva - 1615310025
 #
 
 peso = float(input("Informe o peso dos peixes: "))
@@ -18,7 +16,7 @@ peso = float(input("Informe o peso dos peixes: "))
 if (peso > 50):
     excesso = peso - 50
     multa = excesso * 4.00
-    print("Você excedeu" ,excesso, "kg do numero permitido de peixes \n  O valor \de sua multa e de R$",multa)
+    print("VocÃª excedeu" ,excesso, "kg do numero permitido de peixes \n  O valor \de sua multa e de R$",multa)
 
 else:
     print("Voce nao excedeu o limite de peixes pescados")
