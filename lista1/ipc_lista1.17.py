a8d673474a231cf7760bb4362e0a1505d4328380
Mateus Souza <mota.mateus@live.com>
2016-04-18 11:56:28 -0400

    Update ipc_lista1.17.py

--- lista1/ipc_lista1.17.py
+++ lista1/ipc_lista1.17.py
@@ -11,24 +11,24 @@
 #Victor Rafael da Silva e Silva - 1615310025
 #
 
-import math
+import math # Importando a biblioteca matemática
 
-area = float(input("Area em metros:\n"))
-litros = area/6
+area = float(input("Area em metros:\n")) # Solicita ao usuário a quantidade em metros a ser pintada
+litros = area/6 # Aqui calcula a quantidade em litros necessária
 
-qtdlatas = litros/18
-qtdlatasF = round(qtdlatas)
-valor_latas = qtdlatasF*80
+qtdlatas = litros/18 # Processamento da quantidade de litros a ser utilizados sobre a quantidade de litros de uma lada que determinará a quantidade de latas
+qtdlatasF = round(qtdlatas) # Caso seja um número de ponto flutuante, arredondará o valor
+valor_latas = qtdlatasF*80 # Aqui calculará o preço total pago pelas latas
 
-print("A quantidade de latas sera de:%d"%qtdlatasF)
-print("O valor a ser pago pelas latas sera de:%d"%valor_latas)
+print("A quantidade de latas sera de:%d"%qtdlatasF) # Imprime a quantidade de latas a serem compradas  
+print("O valor a ser pago pelas latas sera de:%d"%valor_latas) # Imprime o preço das latas a serem compradas
 
-qtdgalao = litros/3.6
-qtdgalaoF = round(qtdgalao)
-valor_galao = qtdgalaoF*25
+qtdgalao = litros/3.6 # Processamento de quantidade de litros a ser utilizado sobre a quantidade de litros de um galao que determinará a quantidade de galões
+qtdgalaoF = round(qtdgalao) # Caso seja um número de ponto flutuante, arredondará o valor 
+valor_galao = qtdgalaoF*25 # Calculo do preço pago pelos galões 
 
-print("A quantidade de galoes sera de:%d"%qtdgalaoF)
-print("O valor de galao e:%d"%valor_galao)
+print("A quantidade de galoes sera de:%d"%qtdgalaoF)# Aqui imprimirá a quantidade de galões necessárias
+print("O valor de galao e:%d"%valor_galao) # Aqui imprimirá ao usuário o valor total dos galões
 
 restolata = (litros % 18)
 restogalao = round(restolata/3.6)
