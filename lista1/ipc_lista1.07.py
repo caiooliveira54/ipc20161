8fa565185f19dddd1ecdf96d6ca585e7cda5a16b
Mateus Souza <mota.mateus@live.com>
2016-04-18 11:30:07 -0400

    Update ipc_lista1.07.py

--- lista1/ipc_lista1.07.py
+++ lista1/ipc_lista1.07.py
@@ -11,10 +11,11 @@
 #Victor Rafael da Silva e Silva - 1615310025
 #
    
-altura = float(input("Digite a altura do quadrado em metros: "))
-largura = float(input("Digite a largura do quadrado em metros: "))
+altura = float(input("Digite a altura do quadrado em metros: ")) # Aqui solicita ao usuário a altura do quadrado
+largura = float(input("Digite a largura do quadrado em metros: ")) # Aqui solicita ao usuário a largura do quadrado 
+                                                                  # Obs.: Por ser um quadrado, bastava apenas um lado
 
-a = altura * largura
+a = altura * largura # Processo que calcula a área do quadrado
 
-print ("A area e: %.2f m²" %a)
-print ("O DOBRO da area e:",(a * 2), "m²")
+print ("A area e: %.2f m²" %a) # Impressão ao usuário da área do quadrado
+print ("O DOBRO da area e:",(a * 2), "m²") # Impressão do dobro da área
