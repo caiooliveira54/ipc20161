2c8a45ff4a4d7c616d8c18bda98bcbbb273d6c70
arielrocha1 <ariel_rocha@live.com>
2016-04-18 03:16:37 -0400

    Update ipc_lista2.04.py

--- lista2/ipc_lista2.04.py
+++ lista2/ipc_lista2.04.py
@@ -4,14 +4,14 @@
 #EQUIPE 1
 #
 #Any Mendes Carvalho - 1615310044
-#Kid Mendes de Oliveira Neto - 1615310011
-#Victor Rafael da Silva e Silva - 1615310025
 #Eduardo Maia Freire - 1615310003
+#Kid Mendes de Oliveira Neto - 1615310011
 #Luiz Gustavo de Rocha Melo - 1615310015
 #Matheus Palheta Barbosa -1615310019
+#Victor Rafael da Silva e Silva - 1615310025
 #
 
-letra = raw_input("Informe uma letra: ")
+letra = input("Informe uma letra: ")
 
 if (letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u"):
     print (letra, "e uma vogal")
