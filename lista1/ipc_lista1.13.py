e12467b3fead0c65b7e1c62daf69633868242a4f
Mateus Souza <mota.mateus@live.com>
2016-04-18 12:00:11 -0400

    Update ipc_lista1.13.py

--- lista1/ipc_lista1.13.py
+++ lista1/ipc_lista1.13.py
@@ -11,16 +11,16 @@
 #Victor Rafael da Silva e Silva - 1615310025
 #
 
-altura = float(input("Entre com a sua altura: "))
-sexo = input("Sexo M ou F: ")
-peso = float(input("Qual seu peso: ")) 
+altura = float(input("Entre com a sua altura: ")) # Solicita a altura do usuário
+sexo = input("Sexo M ou F: ") # Logo após isso solicita o sexo do usuário
+peso = float(input("Qual seu peso: "))  # E por último solicita
     
-if ("F" == sexo):          
+if ("F" == sexo): # Verificará se o sexo é feminino, se sim, calculará o peso ideal
      resultado = (62.1*altura) - 44.7  
-else:  
+else:  # Caso seja masculino, calculará o peso ideal
      resultado = (72.7*altura) - 58     
   
-if (peso > resultado):            
+if (peso > resultado): # Se o peso for maior que o peso ideal, imprimirá que está acima do peso
     print("Voce esta acima do peso, seu peso ideal e %.1f kg" % (resultado)) 
-else:
+else: # Caso contrário, abaixo do peso ideal
     print("Voce esta abaixo do peso, seu peso ideal e %.1f kg" % (resultado))
