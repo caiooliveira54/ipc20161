c20214d47901beae6d9e5ae5473f76e675230951
arielrocha1 <ariel_rocha@live.com>
2016-04-18 03:32:19 -0400

    Update ipc_lista2.24.py

--- lista2/ipc_lista2.24.py
+++ lista2/ipc_lista2.24.py
@@ -4,20 +4,20 @@
 #EQUIPE 2
 #
 #Ana Beatriz Frota  - 1615310027 
+#Ariel Guilherme Rocha Capistrano - 1615310029
+#Frankilin Yuri Gonçaves dos santos - 1615310033
 #Kylciane Cristiny Lopes Freitas - 1615310052
-#Franklin Yuri Gonçaves dos Santos - 1615310033
 #Lucas Ferreira Soares - 1615310014
+#Luiz Alexandre Oliveira de Souza - 1615310057
 #Luiz Gustavo Rocha Melo - 1615310015
 #Nahan Trindade Passos - 1615310021
 #Samuel Silva França - 1615310049
-#Luiz Alexandre Oliveira de Souza - 1615310057
-#Ariel Guilherme Rocha Capistrano - 1615310029
 #
 
 numero1 = float(input("Digite o primeiro numero:"))
 numero2 = float(input("Digite o segundo numero:"))
 
-operacao = raw_input("Escolha uma operacao: (+)-Adicao;(-)-Subtracao;(*)-Multiplicacao;(/)-Divisao\n")
+operacao = input("Escolha uma operacao: (+)-Adicao;(-)-Subtracao;(*)-Multiplicacao;(/)-Divisao\n")
 
 if(operacao == "+"):
     adicao = (numero1+numero2)
