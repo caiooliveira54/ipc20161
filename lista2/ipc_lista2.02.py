997c29795303c459df4de43c548f5f93060fcfd2
arielrocha1 <ariel_rocha@live.com>
2016-04-18 03:11:34 -0400

    Update ipc_lista2.02.py

--- lista2/ipc_lista2.02.py
+++ lista2/ipc_lista2.02.py
@@ -1,18 +1,15 @@
 #
-#Introdução a programação de computadores
+#introdução a programação de computadores
 #Professor: Jucimar JR
 #EQUIPE 1
 #
 #Any Mendes Carvalho - 1615310044
-#Kid Mendes de Oliveira Neto - 1615310011
-#Victor Rafael da Silva e Silva - 1615310025
 #Eduardo Maia Freire - 1615310003
+#Kid Mendes de Oliveira Neto - 1615310011
 #Luiz Gustavo de Rocha Melo - 1615310015
 #Matheus Palheta Barbosa -1615310019
+#Victor Rafael da Silva e Silva - 1615310025
 #
-#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
-#
-
 
 valor = float(input("Informe um numero: "))
 
