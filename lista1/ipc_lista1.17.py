acca3fa2b517d0135a8b1e92c3751d70684a178b
arielrocha1 <ariel_rocha@live.com>
2016-04-18 02:59:16 -0400

    Update ipc_lista1.17.py

--- lista1/ipc_lista1.17.py
+++ lista1/ipc_lista1.17.py
@@ -1,20 +1,14 @@
 #
-#Introdução a programação de computadores
+#introdução a programação de computadores
 #Professor: Jucimar JR
 #EQUIPE 1
 #
 #Any Mendes Carvalho - 1615310044
-#Kid Mendes de Oliveira Neto - 1615310011
-#Victor Rafael da Silva e Silva - 1615310025
 #Eduardo Maia Freire - 1615310003
+#Kid Mendes de Oliveira Neto - 1615310011
 #Luiz Gustavo de Rocha Melo - 1615310015
 #Matheus Palheta Barbosa -1615310019
-#
-#Faça um Programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 6 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00 ou em galões de 3,6 litros, que custam R$ 25,00.
-# Informe ao usuário as quantidades de tinta a serem compradas e os respectivos preços em 3 situações:
-#    comprar apenas latas de 18 litros;
-#    comprar apenas galões de 3,6 litros;
-#    misturar latas e galões, de forma que o preço seja o menor. Acrescente 10% de folga e sempre arredonde os valores para cima, isto é, considere latas cheias. 
+#Victor Rafael da Silva e Silva - 1615310025
 #
 
 import math
