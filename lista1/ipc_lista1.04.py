d6a149a2f721e98d0afc6f7b621b31705d7b47ac
Mateus Souza <mota.mateus@live.com>
2016-04-18 11:22:57 -0400

    Update ipc_lista1.04.py

--- lista1/ipc_lista1.04.py
+++ lista1/ipc_lista1.04.py
@@ -11,11 +11,11 @@
 #Victor Rafael da Silva e Silva - 1615310025
 #
 
-nota1 = float(input("Digite a primeira nota do bimestre: "))
-nota2 = float(input("Digite a segunda nota do bimestre: "))
-nota3 = float(input("Digite a terceira nota do bimestre: "))
-nota4 = float(input("Digite a quarta nota do bimestre: "))
+nota1 = float(input("Digite a primeira nota do bimestre: ")) ####
+nota2 = float(input("Digite a segunda nota do bimestre: "))     #
+nota3 = float(input("Digite a terceira nota do bimestre: "))    ### Solicitado as quatro notas do bimestre do aluno/usuário
+nota4 = float(input("Digite a quarta nota do bimestre: "))   ####
 
-media = (nota1+nota2+nota3+nota4)/ 4.0
+media = (nota1+nota2+nota3+nota4)/ 4 # Aqui ocorre o processo de cálculo da média
 
-print ("A sua media e: %s" %media)
+print ("A sua media e: %s" %media) # Ao final, imprime ao usuário sua média final
