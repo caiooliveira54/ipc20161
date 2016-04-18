468d92f88fbafe963ea41b3bbb76301f9d4f9de1
arielrocha1 <ariel_rocha@live.com>
2016-04-18 03:31:05 -0400

    Update ipc_lista2.22.py

--- lista2/ipc_lista2.22.py
+++ lista2/ipc_lista2.22.py
@@ -4,14 +4,14 @@
 #EQUIPE 2
 #
 #Ana Beatriz Frota  - 1615310027 
+#Ariel Guilherme Rocha Capistrano - 1615310029
+#Frankilin Yuri Gonçaves dos santos - 1615310033
 #Kylciane Cristiny Lopes Freitas - 1615310052
-#Franklin Yuri Gonçaves dos Santos - 1615310033
 #Lucas Ferreira Soares - 1615310014
+#Luiz Alexandre Oliveira de Souza - 1615310057
 #Luiz Gustavo Rocha Melo - 1615310015
 #Nahan Trindade Passos - 1615310021
 #Samuel Silva França - 1615310049
-#Luiz Alexandre Oliveira de Souza - 1615310057
-#Ariel Guilherme Rocha Capistrano - 1615310029
 #
 
 numero = int(input("Digite um numero inteiro:"))
