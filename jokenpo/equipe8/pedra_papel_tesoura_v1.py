1ec95ef007c463e345b583a14120678acddca10c
StockRat <mota.mateus@live.com>
2016-04-18 20:39:34 -0400

    Equipe 8

---
+++ jokenpo/equipe8/pedra_papel_tesoura_v1.py
@@ -0,0 +1,36 @@
+import random
+print ("Jogo da velha v1.0 \n")
+
+usuario = str(input("Digite seu nome: \n"))
+print ("Olá " + usuario + " você está afim de uma partida desafiadora?")
+quantidade = int(input("\nQuantas partidades você deseja jogar comigo? \n"))
+
+print ("Agora vamos ver se consegue me vencer... \n1- Pedra \n2- Papel \n3- Tesoura")
+vitoria = 0
+
+for i in range(quantidade):
+    cpu = random.randint(1,3)
+    escolha = int(input("\nQual a sua jogada? \n"))
+    if escolha == 1 and cpu == 1:
+        print ("\nParece que empatamos desta vez "+ usuario +", ambos escolhemos pedra")
+    if escolha == 1 and cpu == 2:
+        print ("\nVocê perdeu " + usuario + ", você escolheu pedra e eu papel")
+    if escolha == 1 and cpu == 3:
+        print ("\nVocê saiu vitorioso desta vez, " + usuario + " por sorte escolhi tesoura e você pedra")
+        vitoria += 1
+    if escolha == 2 and cpu == 1:
+        print ("\nVocê saiu vitorioso desta vez, " + usuario + " por sorte escolhi pedra e você papel")
+        vitoria += 1
+    if escolha == 2 and cpu == 2:
+        print ("\nParece que empatamos desta vez "+ usuario + ", ambos escolhemos papel")
+    if escolha == 2 and cpu == 3:
+        print ("\nVocê perdeu " + usuario + ", você escolheu papel e eu tesoura")
+    if escolha == 3 and cpu == 1:
+        print ("\nVocê perdeu " + usuario + ", você escolheu tesoura e eu pedra")
+    if escolha == 3 and cpu == 2:
+        print ("\nVocê saiu vitorioso desta vez, " + usuario + " por sorte escolhi papel e você tesoura")
+        vitoria += 1
+    if escolha == 3 and cpu == 3:
+        print ("\nParece que empatamos desta vez "+ usuario +", ambos escolhemos tesoura")
+    
+print ("\nQuem sabe não jogamos mais um pouco depois " + usuario +", você venceu", vitoria,"vez(es)")
---
+++ jokenpo/equipe8/pedra_papel_tesoura_v2.py
@@ -0,0 +1,82 @@
+import random
+print ("Pedra, Papel, Tesoura, Spock e Lagarto - v1 \n")
+
+nome = str(input("Qual seu nome? \n"))
+print ("\nOlá " + nome + ", vamos jogar Pedra, Papel, Tesoura, Spock e Lagarto?")
+quantidade = int(input("Quantas vezes você quer me desafiar? \n"))
+vitoria = 0
+print ("\nOpções para escolha: \n1- Pedra \n2- Papel \n3- Tesoura \n4- Spock \n5- Lagarto \n")
+
+for i in range(quantidade):
+    cpu = random.randint(1,5)
+    escolha = int(input("Qual a sua escolha?\n"))
+    if escolha == 1:
+        if cpu == 1:
+            print ("\nAparentemente empatamos " + nome + ", você escolheu pedra e eu também\n")
+        if cpu == 2:
+            print ("\nQue pena " + nome + ", você perdeu, papel cobre pedra\n")
+        if cpu == 3:
+            print ("\nParabéns " + nome + ", você venceu, pedra quebra tesoura\n")
+            vitoria += 1
+        if cpu == 4:
+            print ("\Que pena " + nome + ", Spock evaporiza pedra\n")
+        if cpu == 5:
+            print ("\nCaraca " + nome + ", você decapitou meu lagarto com sua pedra\n")
+            vitoria += 1
+    if escolha == 2:
+        if cpu == 1:
+            print ("\nParabéns" + nome + ", papel cobre pedra\n")
+            vitoria += 1
+        if cpu == 2:
+            print ("\nAparentemente empatamos "+ nome + ", você escolheu papel e eu também\n")
+        if cpu == 3:
+            print ("\nQue pena " + nome + ", tesoura corta papel\n")
+        if cpu == 4:
+            print ("\nCaraca " + nome + ", você refrutou Spock com seu papel\n")
+            vitoria += 1
+        if cpu == 5:
+            print ("\nQue pena " + nome + ", lagarto acidentalmente comeu seu papel\n")
+    if escolha == 3:
+        if cpu == 1:
+            print ("\nQue pena " + nome + ", pedra destrói tesoura\n")
+        if cpu == 2:
+            print ("\nParabéns " + nome + ", tesoura corta papel\n")
+            vitoria += 1
+        if cpu == 3:
+            print ("\nAparentemente empatamos "+ nome + ", você escolheu tesoura e eu também\n")
+        if cpu == 4:
+            print ("\nQue pena " + nome + ", Spock destruiu a sua tesoura\n")
+        if cpu == 5:
+            print ("\nCaraca " + nome + ", você decapitou meu lagarto com sua tesoura\n")
+            vitoria += 1
+    if escolha == 4:
+        if cpu == 1:
+            print ("\nCaramba " + nome + ", Spock vaporizou minha pedra\n")
+            vitoria += 1
+        if cpu == 2:
+            print ("\nCaramba " + nome + ", seu papel refrutou Spock\n")
+            vitoria += 1
+        if cpu == 3:
+            print ("\nQue pena " + nome + ", Spock quebrou sua tesoura\n")
+        if cpu == 4:
+            print ("\nAparentemente empatamos "+ nome + ", você escolheu Spock e eu também\n")
+        if cpu == 5:
+            print ("\Que pena " + nome + ", Spock foi envenenado pelo lagarto\n")
+
+    if escolha == 5:
+        if cpu == 1:
+            print ("\nQue pena " + nome + ", minha pedra quebrou sua\n")
+        if cpu == 2:
+            print ("\nCaramba " + nome + ", seu lagarto comeu meu papel\n")
+            vitoria += 1
+        if cpu == 3:
+            print ("\nQue pena " + nome + ", eu decaptei seu lagarto com a tesoura\n")
+        if cpu == 4:
+            print ("\nCaracoles " + nome + ", seu lagarto envenenou Spock\n")
+            vitoria += 1
+        if cpu == 5:
+            print ("\nAparentemente empatamos "+ nome + ", você escolheu lagarto e eu também\n")
+
+print ("\nObrigado por jogar, quem sabe não tentamos uma partida futuramente, você ganhou", vitoria, "vez(es), parabéns")
+            
+            
