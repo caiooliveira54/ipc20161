4e682165cfb2bb5219186ca14288c35d5aa6032d
arielrocha1 <ariel_rocha@live.com>
2016-04-20 00:49:08 -0400

    Update ipc_lista3.13.py

--- lista3/ipc_lista3.13.py
+++ lista3/ipc_lista3.13.py
@@ -1,10 +1,14 @@
-#Introdução a programação de computadores;
-#Professor: Jucimar Junior
+#
+#introdução a programação de computadores
+#Professor: Jucimar JR
+#EQUIPE 3
+#
+#Antonio Rodrigues de Souza Neto - 1615310028
+#Caio de Oliveira Martins - 1615310031
+#Calebe Roberto Chaves da Silva Rebello - 1615310043
 #Felipe Henrique Bastos Costa - 1615310032
 #Lorene da Silva Marques - 1615310013
-#Caio de Oliveira Martins - 1615310031
-#Antonio Rodrigues de Souza Neto - 1615310028
-#Calebe Roberto Chaves da Silva Rebello - 1615310043
+#
 
 base=int(input("Digite o valor da base:"))
 expoente=int(input("Digite o valor do expoente:"))
@@ -15,4 +19,4 @@ while cont < expoente:
     produto=produto*base
     cont=cont+1
 
-print(base, "elevado a", expoente, "=", produto)    
+print(base, "elevado a", expoente, "=", produto)
