35b0097d9c851e1f944a7c121d011e3c725400e0
arielrocha1 <ariel_rocha@live.com>
2016-04-20 00:50:50 -0400

    Update ipc_lista3.17.py

--- lista3/ipc_lista3.17.py
+++ lista3/ipc_lista3.17.py
@@ -1,10 +1,14 @@
-#Introdução a programação de computadores;
-#Professor: Jucimar Junior
+#
+#introduÃ§Ã£o a programaÃ§Ã£o de computadores
+#Professor: Jucimar JR
+#EQUIPE 3
+#
+#Antonio Rodrigues de Souza Neto - 1615310028
+#Caio de Oliveira Martins - 1615310031
+#Calebe Roberto Chaves da Silva Rebello - 1615310043
 #Felipe Henrique Bastos Costa - 1615310032
 #Lorene da Silva Marques - 1615310013
-#Caio de Oliveira Martins - 1615310031
-#Antonio Rodrigues de Souza Neto - 1615310028
-#Calebe Roberto Chaves da Silva Rebello - 1615310043
+#
 
 n = int(input("Digite o valor de n: "))
 
