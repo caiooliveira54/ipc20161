2d6d77a4320e9b1497ba091cfee37436f7ded3ce
arielrocha1 <ariel_rocha@live.com>
2016-04-22 04:24:16 -0400

    Update ipc_lista3.29.py

--- lista3/ipc_lista3.29.py
+++ lista3/ipc_lista3.29.py
@@ -1,11 +1,16 @@
-#Introduçao a programaçao de computadores
-#Professor: Jucimar Junior
+#
+#introdução a programação de computadores
+#Professor: Jucimar JR
+#EQUIPE 4
+#
 #Bruno de Oliveira Freire - 1615310030
-#Thiago Santos Borges - 1615310023
-#Nickso Patrick Façanha Calheiros - 1615310059
-#Matheus Mota de Souza - 1615310016
 #Igor Menezes Sales Vieira - 1615310007
-#Nadine Da Cunha Brito - 1615310040
+#Matheus Mota de Souza - 1615310016
+#Nadine da Cunha Brito - 1615310040
+#Nickso Patrick Façanha Calheiros - 1615310059
+#Thiago Santos Borges - 1615310023
+#
+
 preco=1.99
 cont=1
 soma=0
@@ -16,5 +21,3 @@ while(cont<=50):
     nume+=1
     print("%d R$%.2f"%(nume,soma))
     cont+=1
-    
-    
