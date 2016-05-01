e78da4e67b8df3905f61f6701305d3b6dad80eab
Nickso Calheiros <nicksofacanha@gmail.com>
2016-05-01 16:00:00 -0400

    Update ipc_lista5.06.py

--- lista5/ipc_lista5.06.py
+++ lista5/ipc_lista5.06.py
@@ -1,30 +1,16 @@
 #Nickso Patrick Façanha Calheiros - 1615310059
 #
 #
+
 from matriz import*
 
-def verificar_nulas (matriz,linha, coluna):
-    ac_linha = 0
-    ac_coluna = 0
-    for i in range(linha):
-        ac1 = 0
-        ac2 = 0
-        for j in range(coluna):
-            if matriz[i][j] == 0:
-                ac1 += 1
-                if ac1 == linha:
-                    ac_linha += 1
-            if matriz[j][i] == 0:
-                ac2 += 1
-                if ac2 == coluna:
-                    ac_coluna += 1
-    return ("Numero de Linhas nulas %d, e numero de colunas nulas %d" %(ac_linha, ac_coluna))
-m=int(input())
-n=int(input())
+m=int(input("Numero de linhas: "))
+n=int(input("Numero de colunas: "))
+
 matriz = []
 matriz = gerar_matriz(m,n)
+
+mostrar_matriz(matriz,m)
+
 x = verificar_nulas(matriz,m,n)
 print (x)
-           
-                
-            
