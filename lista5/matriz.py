cf0f7acca8f425e0a084eea84543e77db33de775
Nickso Calheiros <nicksofacanha@gmail.com>
2016-05-01 15:49:50 -0400

    Update matriz.py

--- lista5/matriz.py
+++ lista5/matriz.py
@@ -26,6 +26,24 @@ def verificar_permutacao (matriz, linha, coluna):
         else:
             return("Nao e permutaçao")
 
+def verificar_nulas (matriz,linha, coluna):
+    ac_linha = 0
+    ac_coluna = 0
+    for i in range(linha):
+        ac1 = 0
+        ac2 = 0
+        for j in range(coluna):
+            if j < linha and i < coluna:
+                if matriz[i][j] == 0:
+                    ac1 += 1
+                    if ac1 == linha or ac1 == coluna:
+                        ac_linha += 1
+            if j < linha and i < coluna:
+                if matriz[j][i] == 0:
+                    ac2 += 1
+                    if ac2 == coluna or ac2 == linha:
+                        ac_coluna += 1
+    return ("Numero de Linhas nulas %d, e numero de colunas nulas %d" %(ac_linha, ac_coluna))
 
 def mostrar_matriz(matriz, linhas):
 	for i in range(linhas):
