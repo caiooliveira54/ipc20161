84fcbe33e101527e90ba10262048c346cc290cd8
Kid Mendes <kid.mon@hotmail.com>
2016-05-02 11:12:04 -0400

    Lista 5 - Ex.08 >> Comentada

--- lista5/ipc_lista5.08.py
+++ lista5/ipc_lista5.08.py
@@ -7,27 +7,27 @@
 #Igor Menezes Sales Vieira - 1615310007
 #
 
-def fatorial(n):
+def fatorial(n):                                        #Fatoração Numérica
     if n == 1 or n == 0:
         return 1
     else:
         return n * fatorial(n-1)
 
-def analise_c(n,i):
+def analise_c(n,i):                                     #Análise Combinatória
     ac = fatorial(n)/(fatorial(i)*fatorial(n-i))
     return ac
 
 
-n = int(input("Digite um valor de n:\n"))
+n = int(input("Digite um valor de n:\n"))               #Entrada
 vetor = []
 
-for n in range(0,n+1):
+for n in range(0,n+1):                                  #Processamento
     vet = []
     for i in range(0,n+1):
         vet.append(analise_c(n,i))
         vetor.append(analise_c(n,i))
-    print(vet)
-print(vetor)
+    print(vet)                                          #Triângulo de Pascal
+print(vetor)                                            #Triângulo de Pascal apenas em um vetor
         
     
     
