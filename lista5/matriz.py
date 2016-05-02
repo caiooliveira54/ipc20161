9fbeca3093cef10909a68d0926030c58411be729
Nickso Calheiros <nicksofacanha@gmail.com>
2016-05-02 00:28:11 -0400

    Update matriz.py

--- lista5/matriz.py
+++ lista5/matriz.py
@@ -84,3 +84,40 @@ def verificar_repeticao(matriz,linhas,colunas):
         return "Nao existem elementos repetidos na matriz"
     else:
         return "Existem elementos repetidos na matriz"
+
+def verificar_linhas(matriz, linha, coluna, acms):
+    acms = []
+    for i in range(linha):
+        acm = 0
+        for j in range(coluna):
+            acm += matriz[i][j]
+        acms.append(acm)
+    return (acms)
+
+def verificar_colunas(matriz, linha, coluna, acms):
+    acms = []
+    for i in range(linha):
+        acm = 0
+        for j in range(coluna):
+            acm += matriz[j][i]
+        acms.append(acm)
+    return acms
+
+def verificar_diagonais(matriz, linha, coluna, acm, acm1):
+    for i in range(linha):
+        for j in range(coluna):
+            if (i == j):
+                acm += matriz[i][j]
+            if ((i+j) == (linha-1)):
+                acm1 += matriz[i][j]
+    return acm, acm1
+    
+def verificar_cubo(lista):
+    cont = 0
+    for i in lista:
+        if i == lista[0]:
+            cont += 1
+    if cont == len(lista):
+        print ("É um cubo mágico")
+    else:
+        print ("Não é um cubo mágico")
