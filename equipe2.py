cc534aca583e4411c50187c9faf0875a33baae88
tsbecn16 <tsb.ecn16@uea.edu.br>
2016-05-03 23:38:33 -0400

    Update and rename equipe2.txt to equipe2.py

--- equipe2.py
+++ equipe2.py
@@ -1,5 +1,6 @@
-nadine da cunha brito - 1615310040
-samuel silva de frança - 1615310049
+#encoded=UTF-8
+"""nadine da cunha brito - 1615310040
+samuel silva de franÃ§a - 1615310049
 antonio rodrigues de souza neto - 1615310028
 thiago santos borges - 1615310023
 caio de oliveira martins - 1615310031
@@ -7,5 +8,6 @@ bruno de oliveira freire - 1615310030
 calebe roberto chaves da silva rebello - 1615310043
 ana jessye almeida antunes - 1615310046
 luiz alexandre olveira de souza - 1615310057
-franklin yuri gonçalves dos santos - 1615310033
+franklin yuri gonÃ§alves dos santos - 1615310033
 kylciane cristiny lopes freitas - 1615310052
+ana beatriz frota - 1615310027"""
