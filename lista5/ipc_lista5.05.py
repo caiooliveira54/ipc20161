baa9804b24d5f8f044a2ef1d7dc32792af5c444b
AbFrota <beatrizffrotaa@gmail.com>
2016-05-11 18:05:53 -0400

    Update ipc_lista5.05.py

--- lista5/ipc_lista5.05.py
+++ lista5/ipc_lista5.05.py
@@ -10,6 +10,6 @@ m = int(input("quantidade de linhas: " ))
 n = int(input("quantidade de colunas: "))
 matriz = []
 matriz = gerar_matriz(m,n)
-montar_matriz(matriz, m)
+mostrar_matriz(matriz, m)
 x = verificar_permutacao(matriz,m,n)
 print(x)
