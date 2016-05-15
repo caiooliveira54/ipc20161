d66e18ce06f340696e9c8a6e25125598728125b7
arielrocha1 <ariel_rocha@live.com>
2016-05-15 16:46:02 -0400

    Update matriz.py

--- lista5/equipe5/matriz.py
+++ lista5/equipe5/matriz.py
@@ -111,3 +111,34 @@ def verificar_nulas (matriz,linha,coluna):
                     if ac2 == coluna or ac2 == linha:
                         ac_coluna += 1
     print("Numero de Linhas nulas %d, e numero de colunas nulas %d" %(ac_linha, ac_coluna))
+
+def chamar_numero():
+    numero = int(input("digite um numero: "))
+    return numero
+
+def fatorar_numero(numero):
+    if (numero == 1) or (numero == 0):
+        return 1
+    else:
+        n = numero - 1
+        return numero * fatorar_numero(n)
+    
+def analisar_combinacao(numero,acm):
+    resultado = fatorar_numero(numero) / (fatorar_numero(acm) * fatorar_numero(numero - acm))
+    return resultado
+
+def fazer_triangulo(numero):
+    for numero in range(0,numero + 1):
+        triangulo = []
+        for acm in range(0,numero + 1):
+            triangulo.append(int(analisar_combinacao(numero, acm)))
+        print (triangulo)
+
+def botar_vetor(numero):
+    vetor = []
+    for numero in range(0,numero + 1):
+        triangulo = []
+        for acm in range(0,numero + 1):
+            resultado = int(fatorar_numero(numero) / (fatorar_numero(acm) * fatorar_numero(numero - acm)))
+            vetor.append(resultado)
+    print(vetor)
