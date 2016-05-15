50afad701ab3e2fe09788b724eb23497d9572a65
arielrocha1 <ariel_rocha@live.com>
2016-05-15 16:45:44 -0400

    Update ipc_lista5.08.py

--- lista5/equipe5/ipc_lista5.08.py
+++ lista5/equipe5/ipc_lista5.08.py
@@ -5,39 +5,8 @@
 #
 #Ariel Guilherme Rocha Capistrano - 1615310029
 #
-##bug ao importa do matriz.py, por isso não botarei.
-#
 
-def chamar_numero():
-    numero = int(input("digite um numero: "))
-    return numero
-
-def fatorar_numero(numero):
-    if (numero == 1) or (numero == 0):
-        return 1
-    else:
-        n = numero - 1
-        return numero * fatorar_numero(n)
-    
-def analisar_combinacao(numero,acm):
-    resultado = fatorar_numero(numero) / (fatorar_numero(acm) * fatorar_numero(numero - acm))
-    return resultado
-
-def fazer_triangulo(numero):
-    for numero in range(0,numero + 1):
-        triangulo = []
-        for acm in range(0,numero + 1):
-            triangulo.append(int(analisar_combinacao(numero, acm)))
-        print (triangulo)
-
-def botar_vetor(numero):
-    vetor = []
-    for numero in range(0,numero + 1):
-        triangulo = []
-        for acm in range(0,numero + 1):
-            resultado = int(fatorar_numero(numero) / (fatorar_numero(acm) * fatorar_numero(numero - acm)))
-            vetor.append(resultado)
-    print(vetor)
+from matriz import*
 
 numero = chamar_numero()
 fazer_triangulo(numero)
