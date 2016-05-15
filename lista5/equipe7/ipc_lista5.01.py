93cb7a0bcf2b9ff79eac0ee83d19c203d890b650
Ana Jessye <gustavo.gurjao@hotmail.com>
2016-05-15 16:44:44 -0400

    Add files via upload

---
+++ lista5/equipe7/ipc_lista5.01.py
@@ -0,0 +1,59 @@
+#Introducao a programacao de computadores
+#Professor: Jucimar Junior
+
+
+
+#Questao1. Dada uma matriz real A com m linhas e n colunas e um vetor real V com n elementos, determinar o produto de A por V. 
+
+def multiplicarvetor(m,n):
+    matrizR = []
+    for i in range (m):
+        linha = []
+        for j in range (n):
+            mul = A[i][j] * V [i]
+            linha.append(mul)
+        matrizR.append(linha)
+    vetorR = []
+    h = 0    
+    for k in range(m):
+        num = matrizR[k][h] + matrizR[k][h+1]
+        vetorR.append(num)
+    return vetorR
+
+def fazermatriz (m,n):
+    matriz =[]
+    for i in range (1, m+1):
+        linha = []
+        for j in range (1, n+1):
+            num = int(input("Digite a posicao %i%i da matriz: " %(i,j)))
+            linha.append(num)
+        matriz.append(linha)
+    return matriz
+        
+def fazervetor(n):
+    vetor = []
+    for i in range (1, n+1):
+        num = int (input("Digite o elemento %i do vetor: " %(i)))
+        vetor.append(num)
+    return vetor
+      
+def mostrarmatriz(m, n):
+    for i in range(m):
+        print (A[i])
+        
+        
+        
+m = int(input("Digite a quantidade de linhas da matriz: "))
+n = int(input("Digite a quantidade de colunas da matriz: "))
+A = []
+A = fazermatriz (m,n)
+mostrarmatriz(m, n)
+        
+r = 0
+V = []
+V = fazervetor(n)
+print (V)
+
+vetorR = []
+vetorR = multiplicarvetor(m,n)
+print ("Resultado final da multiplicacao", vetorR)
