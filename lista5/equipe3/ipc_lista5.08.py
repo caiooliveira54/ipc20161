6612a37d20bc5ee152f2a7a1a844df7eaf977c03
samucassf <samuca.dota@outlook.com>
2016-05-15 20:52:42 -0400

    ipc_lista5.08.py

---
+++ lista5/equipe3/ipc_lista5.08.py
@@ -0,0 +1,17 @@
+def fatorar(num):
+    if (num == 1) or (num == 0):
+        return 1
+    else:
+        return num * fatorar(num-1)
+
+def anal_comb(num, i):
+    ac = fatorar(num) / (fatorar(i) * fatorar(num - i))
+    return ac
+
+num = int (input ("Qual o limite? "))
+
+for num in range(0, num+1):
+    triangulo = []
+    for i in range(0, num+1):
+        triangulo.append(int(anal_comb(num, i)))
+    print (triangulo)
