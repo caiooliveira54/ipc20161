53ee176c75e93bd1fbfc88825c84b1e7b90d1dde
arielrocha1 <ariel_rocha@live.com>
2016-05-15 22:50:24 -0400

    Update ipc_lista5.09.py

--- lista5/equipe5/ipc_lista5.09.py
+++ lista5/equipe5/ipc_lista5.09.py
@@ -1,3 +1,11 @@
+#
+#introdução a programação de computadores
+#Professor: Jucimar JR
+#EQUIPE 5
+#Nickso Patrick Façanha Calheiros - 1615310059
+#Ariel Guilherme Rocha Capistrano - 1615310029
+#
+
 from matriz import*
 
 linha = int(input("Quantidade de linhas na matriz: "))
