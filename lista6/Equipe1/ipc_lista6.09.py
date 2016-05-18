ffb5ebad738e434923e339e2c679cbb098de88ce
soAl16 <alexandre.souza120@hotmail.com>
2016-05-18 18:19:00 -0400

    Create ipc_lista6.09.py

---
+++ lista6/Equipe1/ipc_lista6.09.py
@@ -0,0 +1,13 @@
+
+
+def somatorio(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return n + somatorio(n-1)
+
+print("Digite o numero voce deseja somar: ")
+n = int(input("(Vamos soma-lo de 1, ate o numero que voce digitou):"))
+print(somatorio(n))
