e64787428bc380e968e6ec325457b61a7e90535b
Kid Mendes <kid.mon@hotmail.com>
2016-05-19 11:53:14 -0400

    Delete ipc_lista6.01.py

--- lista6/Equipe3/ipc_lista6.01.py
+++
@@ -1,8 +0,0 @@
-def fatorial(num):
-    if(num == 0 or num == 1):
-        return 1
-    else:
-        return num * fatorial(num-1)
-
-num = int(input("Digite um numero:\n"))
-print("O fatorial do numero %s,sera: %s"%(num,fatorial(num)))
