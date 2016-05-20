3129855507512f1142c6bb138530471abea06c41
Nickso Calheiros <nicksofacanha@gmail.com>
2016-05-20 01:48:31 -0400

    Update ipc_lista6.03.py

--- lista6/Equipe1/ipc_lista6.03.py
+++ lista6/Equipe1/ipc_lista6.03.py
@@ -1,3 +1,10 @@
+#Introdução a Programação de Computadores
+#Professor: Jucimar JR
+#EQUIPE 1
+#Nickso Patrick Façanha Calheiros - 1615310059
+#Ariel Guilherme Rocha Capistrano - 1615310029
+#Luiz Alexandre Oliveira de Souza - 1615310057
+
 def inverter (n):
     ac = 0
     if len (str(n)) == 1:
