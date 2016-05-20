639b3f5e360811b5f9386ad81aa30aa11d40b733
Nickso Calheiros <nicksofacanha@gmail.com>
2016-05-20 01:48:03 -0400

    Update ipc_lista6.02.py

--- lista6/Equipe1/ipc_lista6.02.py
+++ lista6/Equipe1/ipc_lista6.02.py
@@ -1,6 +1,6 @@
 #Introdução a Programação de Computadores
 #Professor: Jucimar JR
-#EQUIPE 5
+#EQUIPE 1
 #Nickso Patrick Façanha Calheiros - 1615310059
 #Ariel Guilherme Rocha Capistrano - 1615310029
 #Luiz Alexandre Oliveira de Souza - 1615310057
