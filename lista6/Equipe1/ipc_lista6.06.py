3f7c5859858f1d81c87293f3d99ed82db16325fe
Nickso Calheiros <nicksofacanha@gmail.com>
2016-05-20 01:50:12 -0400

    Update ipc_lista6.06.py

--- lista6/Equipe1/ipc_lista6.06.py
+++ lista6/Equipe1/ipc_lista6.06.py
@@ -1,3 +1,10 @@
+#Introdução a Programação de Computadores
+#Professor: Jucimar JR
+#EQUIPE 1
+#Nickso Patrick Façanha Calheiros - 1615310059
+#Ariel Guilherme Rocha Capistrano - 1615310029
+#Luiz Alexandre Oliveira de Souza - 1615310057
+
 def potenciacao(k,n):
     if n == 0:
         return 1
