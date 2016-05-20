2dd1bf4a9129d17f0a3d8044f969604468aa68da
Nickso Calheiros <nicksofacanha@gmail.com>
2016-05-20 01:49:13 -0400

    Update ipc_lista6.05.py

--- lista6/Equipe1/ipc_lista6.05.py
+++ lista6/Equipe1/ipc_lista6.05.py
@@ -1,3 +1,10 @@
+#Introdução a Programação de Computadores
+#Professor: Jucimar JR
+#EQUIPE 1
+#Nickso Patrick Façanha Calheiros - 1615310059
+#Ariel Guilherme Rocha Capistrano - 1615310029
+#Luiz Alexandre Oliveira de Souza - 1615310057
+
 def somar_n (n):
     if n == 1:
         return 1
