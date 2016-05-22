dc21b287548d0f324dccca3da53c60acfcaf3ca9
annefreitas <annelopesfreitas@gmail.com>
2016-05-22 15:52:59 -0400

    Update ipc_lista6.17.py

--- lista6/Equipe5/ipc_lista6.17.py
+++ lista6/Equipe5/ipc_lista6.17.py
@@ -1,3 +1,10 @@
+
+#Introducao a programacao de computadores
+#Professor: Jucimar Junior
+#Ana Jessye Almeida Antunes- 1615310046
+#Kylciane Cristiny Lopes Freitas - 1615310052
+#Franklin Yuri Goncalves dos Santos - 1615310033
+
 def fatorial (num):
     if num == 1 or num == 0:
         return 1
