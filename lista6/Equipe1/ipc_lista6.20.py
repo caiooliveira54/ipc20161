0cbbf6666aad2223f272b63234c28a8875323581
arielrocha1 <ariel_rocha@live.com>
2016-05-21 21:11:07 -0400

    Update ipc_lista6.20.py

--- lista6/Equipe1/ipc_lista6.20.py
+++ lista6/Equipe1/ipc_lista6.20.py
@@ -1,3 +1,10 @@
+#Introdução a Programação de Computadores
+#Professor: Jucimar JR
+#EQUIPE 1
+#Ariel Guilherme Rocha Capistrano - 1615310029
+#Luiz Alexandre Oliveira de Souza - 1615310057
+#Nickso Patrick Façanha Calheiros - 1615310059
+
 def fatorial_exponencial(numero):
     if numero ==  1:
         return 1
