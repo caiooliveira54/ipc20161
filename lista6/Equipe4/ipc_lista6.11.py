4ef8c5033143c75aaaecacb45ebc144dc627c03c
Mateus Souza <mota.mateus@live.com>
2016-05-23 09:43:20 -0400

    Update ipc_lista6.11.py

--- lista6/Equipe4/ipc_lista6.11.py
+++ lista6/Equipe4/ipc_lista6.11.py
@@ -1,19 +1,21 @@
 def multiplicar(numero, mult):
     if mult == -1:
         return -numero
-    if mult < 0:
-        return numero - multiplicar(numero, mult+1)
     if mult == 1:
         return numero
-    if mult == 0:
+    if mult == 0 or numero == 0:
         return 0
-    if numero == 0:
-        return 0
-    if numero > 0:
-        return numero + multiplicar(numero, mult-1)
+    else:
+        if mult < 0:
+            return -numero + multiplicar(numero, mult+1)
+        
+        if mult > 0:
+            return numero + multiplicar(numero, mult-1)
 
+        
 print ("Mutiplicador de numeros inteiros")
 numero = int(input("Qual o numero a ser multiplicado? "))
 mult = int(input("Quantas vezes esse numero deve ser multiplicado? "))
 produto = multiplicar(numero, mult)
 print ("\n%dx%d ="%(numero, mult), produto)
+
